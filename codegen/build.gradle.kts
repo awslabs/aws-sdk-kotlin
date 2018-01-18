88695ff256e664e22a71f1d84731aa0aaa461345
Kyle Thomson <kylthoms@amazon.com>
2018-01-18 11:28:40 -0800

    Removing unnecessary dependency

--- codegen/build.gradle.kts
+++ codegen/build.gradle.kts
@@ -2,6 +2,5 @@
 dependencies {
     compile("software.amazon.awssdk", "codegen", "2.0.0-preview-7")
     compile("com.squareup:kotlinpoet:0.6.0")
-    compile("software.amazon.awssdk", "polly", "2.0.0-preview-7") //remove this when done testing
 }
 
