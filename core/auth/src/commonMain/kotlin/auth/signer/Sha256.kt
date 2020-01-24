6350461edf05187fcee7ece6390831716498b9a9
Austin Brooks <abrooksv@gmail.com>
2020-01-23 22:01:39 -0800

    Flesh out SHA256 JVM

--- core/auth/src/commonMain/kotlin/auth/signer/Sha256.kt
+++ core/auth/src/commonMain/kotlin/auth/signer/Sha256.kt
@@ -3,4 +3,9 @@ package auth.signer
 expect class Sha256() {
     fun update(data: ByteArray)
     fun digest(): ByteArray
+    fun reset()
+}
+
+expect class HmacSha256(secret: ByteArray) {
+    fun sign(data: ByteArray): ByteArray
 }
