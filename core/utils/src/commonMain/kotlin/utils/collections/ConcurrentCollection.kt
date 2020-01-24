ba2f4fa1d3290be2c6627e5a4d80ee48c08a9f82
Austin Brooks <abrooksv@gmail.com>
2020-01-23 21:59:30 -0800

    Make ConcurrentCollection compile

--- core/utils/src/commonMain/kotlin/utils/collections/ConcurrentCollection.kt
+++ core/utils/src/commonMain/kotlin/utils/collections/ConcurrentCollection.kt
@@ -1,6 +1,7 @@
 package utils.collections
 
 import utils.concurrent.Lock
+import utils.concurrent.withLock
 
 // Original code from https://github.com/ktorio/ktor/blob/3ee2ae68734ab6d3740ec5ba8e978c2b5502e72d/ktor-utils/common/src/io/ktor/util/collections/ConcurrentCollection.kt
 open class ConcurrentCollection<E> internal constructor(
