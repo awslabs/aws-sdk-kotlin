58d34851be4c6bb950ab639518cd1f2dcfdda9fa
Ken Gilmer <kggilmer@amazon.com>
2020-10-30 19:42:51 -0700

    Add service config integrations for aws regions and credentials.

--- codegen/smithy-aws-kotlin-codegen/src/main/kotlin/software/amazon/smithy/aws/kotlin/codegen/AwsServiceConfigIntegration.kt
+++ codegen/smithy-aws-kotlin-codegen/src/main/kotlin/software/amazon/smithy/aws/kotlin/codegen/AwsServiceConfigIntegration.kt
@@ -1,41 +1,72 @@
 package software.amazon.smithy.aws.kotlin.codegen
 
-import software.amazon.smithy.codegen.core.Symbol
 import software.amazon.smithy.codegen.core.SymbolProvider
-import software.amazon.smithy.codegen.core.SymbolReference
 import software.amazon.smithy.kotlin.codegen.*
 import software.amazon.smithy.kotlin.codegen.integration.KotlinIntegration
 import software.amazon.smithy.model.Model
-import software.amazon.smithy.model.shapes.ServiceShape
 import software.amazon.smithy.model.shapes.Shape
 import software.amazon.smithy.model.shapes.ShapeType
 
 class AwsServiceConfigIntegration : KotlinIntegration {
 
-    override val serviceClientConfigFeatures: List<ServiceConfigFeature>
-        get() = listOf(AwsServiceConfigFeature())
+    override fun onShapeWriterUse(
+        settings: KotlinSettings,
+        model: Model,
+        symbolProvider: SymbolProvider,
+        writer: KotlinWriter,
+        definedShape: Shape
+    ) {
+        if (definedShape.type == ShapeType.SERVICE) {
+            writer.addImport("AuthConfig", AwsKotlinDependency.AWS_CLIENT_RT_AUTH)
+            writer.addImport("AwsCredentialsProviders", AwsKotlinDependency.AWS_CLIENT_RT_AUTH)
+            writer.addImport("RegionConfig", AwsKotlinDependency.AWS_CLIENT_RT_REGIONS)
 
-    class AwsServiceConfigFeature : ServiceConfigFeature {
-        override fun supplyConstructorParameters(model: Model, service: ServiceShape, applicationProtocol: ApplicationProtocol, writer: KotlinWriter): List<String> {
-            val awsServiceConfigSymbol = Symbol.builder()
-                    .name("AwsCredentialsProviders")
-                    .namespace(AwsKotlinDependency.AWS_CLIENT_RT_CORE.namespace, ".")
-                    .addDependency(AwsKotlinDependency.AWS_CLIENT_RT_CORE)
-                    .build()
-            writer.addImport(awsServiceConfigSymbol, "", SymbolReference.ContextOption.DECLARE)
+            writer.onSection(SECTION_SERVICE_CONFIG_PARENT_TYPE) { text -> writer.appendWithDelimiter(text, "AuthConfig") }
+            writer.onSection(SECTION_SERVICE_CONFIG_PARENT_TYPE) { text -> writer.appendWithDelimiter(text, "RegionConfig") }
 
-            return listOf("override var credentialProviders: AwsCredentialsProviders? = null", "override var region: String? = null")
+            writer.onSection(SECTION_SERVICE_CONFIG_PROPERTIES) {
+                writer.write(it)
+                writer.write("override val credentialProviders: AwsCredentialsProviders? = builder.credentialProviders")
+                writer.write("override val region: String? = builder.region")
+                writer.write("override val signingRegion: String? = builder.signingRegion")
+            }
+
+            writer.onSection(SECTION_SERVICE_CONFIG_BUILDER_BODY) {
+                writer.write(it)
+                writer.write("fun credentialProviders(credentialProviders: AwsCredentialsProviders): Builder")
+                writer.write("fun region(region: String): Builder")
+                writer.write("fun signingRegion(signingRegion: String): Builder")
+            }
+
+            writer.onSection(SECTION_SERVICE_CONFIG_DSL_BUILDER_BODY) {
+                writer.write(it)
+                writer.write("var credentialProviders: AwsCredentialsProviders?")
+                writer.write("var region: String?")
+                writer.write("var signingRegion: String?")
+            }
+
+            writer.onSection(SECTION_SERVICE_CONFIG_BUILDER_IMPL_PROPERTIES) {
+                writer.write(it)
+                writer.write("override var credentialProviders: AwsCredentialsProviders? = null")
+                writer.write("override var region: String? = null")
+                writer.write("override var signingRegion: String? = null")
+            }
+
+            writer.onSection(SECTION_SERVICE_CONFIG_BUILDER_IMPL_CONSTRUCTOR) {
+                writer.write(it)
+                writer.write("this.credentialProviders = config.credentialProviders")
+                writer.write("this.region = config.region")
+                writer.write("this.signingRegion = config.signingRegion")
+            }
+
+            writer.onSection(SECTION_SERVICE_CONFIG_BUILDER_IMPL_BODY) {
+                writer.write(it)
+                writer.write("override fun credentialProviders(credentialProviders: AwsCredentialsProviders): Builder = apply { this.credentialProviders = credentialProviders }")
+                writer.write("override fun region(region: String): Builder = apply { this.region = region }")
+                writer.write("override fun signingRegion(signingRegion: String): Builder = apply { this.signingRegion = signingRegion }")
+            }
         }
 
-        override fun supplyInterfaces(model: Model, service: ServiceShape, applicationProtocol: ApplicationProtocol, writer: KotlinWriter): List<String> {
-            val awsServiceConfigSymbol = Symbol.builder()
-                    .name("AwsServiceConfig")
-                    .namespace(AwsKotlinDependency.AWS_CLIENT_RT_CORE.namespace, ".")
-                    .addDependency(AwsKotlinDependency.AWS_CLIENT_RT_CORE)
-                    .build()
-            writer.addImport(awsServiceConfigSymbol, "", SymbolReference.ContextOption.DECLARE)
-
-            return listOf("AwsServiceConfig")
-        }
+        super.onShapeWriterUse(settings, model, symbolProvider, writer, definedShape)
     }
 }
