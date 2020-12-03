a572705e3b6a4792c9a88df510915614c303cefb
Aaron J Todd <todaaron@amazon.com>
2020-12-03 13:13:10 -0500

    fix(codegen): override the unit test generation to account for AWS region configuration

---
+++ codegen/smithy-aws-kotlin-codegen/src/main/kotlin/software/amazon/smithy/aws/kotlin/codegen/AwsHttpProtocolUnitTestRequestGenerator.kt
@@ -0,0 +1,28 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0.
+ */
+
+package software.amazon.smithy.aws.kotlin.codegen
+
+import software.amazon.smithy.kotlin.codegen.integration.HttpProtocolUnitTestGenerator
+import software.amazon.smithy.kotlin.codegen.integration.HttpProtocolUnitTestRequestGenerator
+import software.amazon.smithy.protocoltests.traits.HttpRequestTestCase
+
+/**
+ * Override request unit tests to configure AWS specific behaviors
+ */
+class AwsHttpProtocolUnitTestRequestGenerator(builder: Builder) :
+    HttpProtocolUnitTestRequestGenerator(builder) {
+    override fun renderConfigureServiceClient(test: HttpRequestTestCase) {
+        super.renderConfigureServiceClient(test)
+        // specify a default region
+        writer.write("region = \"us-east-1\"")
+    }
+
+    open class Builder : HttpProtocolUnitTestRequestGenerator.Builder() {
+        override fun build(): HttpProtocolUnitTestGenerator<HttpRequestTestCase> {
+            return AwsHttpProtocolUnitTestRequestGenerator(this)
+        }
+    }
+}
---
+++ codegen/smithy-aws-kotlin-codegen/src/main/kotlin/software/amazon/smithy/aws/kotlin/codegen/AwsHttpProtocolUnitTestResponseGenerator.kt
@@ -0,0 +1,46 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0.
+ */
+
+package software.amazon.smithy.aws.kotlin.codegen
+
+import software.amazon.smithy.kotlin.codegen.integration.HttpProtocolUnitTestErrorGenerator
+import software.amazon.smithy.kotlin.codegen.integration.HttpProtocolUnitTestGenerator
+import software.amazon.smithy.kotlin.codegen.integration.HttpProtocolUnitTestResponseGenerator
+import software.amazon.smithy.protocoltests.traits.HttpResponseTestCase
+
+/**
+ * Override response unit tests to configure AWS specific behaviors
+ */
+open class AwsHttpProtocolUnitTestResponseGenerator(builder: Builder) : HttpProtocolUnitTestResponseGenerator(builder) {
+    override fun renderConfigureServiceClient(test: HttpResponseTestCase) {
+        super.renderConfigureServiceClient(test)
+        // specify a default region
+        writer.write("region = \"us-east-1\"")
+    }
+
+    open class Builder : HttpProtocolUnitTestResponseGenerator.Builder() {
+        override fun build(): HttpProtocolUnitTestGenerator<HttpResponseTestCase> {
+            return AwsHttpProtocolUnitTestResponseGenerator(this)
+        }
+    }
+}
+
+/**
+ * Override error unit tests to configure AWS specific behaviors
+ */
+class AwsHttpProtocolUnitTestErrorGenerator(builder: Builder) : HttpProtocolUnitTestErrorGenerator(builder) {
+
+    override fun renderConfigureServiceClient(test: HttpResponseTestCase) {
+        super.renderConfigureServiceClient(test)
+        // specify a default region
+        writer.write("region = \"us-east-1\"")
+    }
+
+    class Builder : HttpProtocolUnitTestErrorGenerator.Builder() {
+        override fun build(): HttpProtocolUnitTestGenerator<HttpResponseTestCase> {
+            return AwsHttpProtocolUnitTestErrorGenerator(this)
+        }
+    }
+}
