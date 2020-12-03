4a601e0e3183fe62deb41b7f3e7b8b10e8b9e191
Aaron J Todd <todaaron@amazon.com>
2020-12-03 11:03:13 -0500

    refactor: rename unknown service error; improve handling of exceptions from deserialization errors

--- client-runtime/aws-client-rt/common/src/software/aws/kotlinsdk/Exceptions.kt
+++ client-runtime/aws-client-rt/common/src/software/aws/kotlinsdk/Exceptions.kt
@@ -10,45 +10,46 @@ import software.aws.clientrt.ServiceException
 /**
  * Base class for all modeled service exceptions
  */
-open class AwsServiceException : ServiceException {
+public open class AwsServiceException : ServiceException {
 
-    constructor() : super()
+    public constructor() : super()
 
-    constructor(message: String?) : super(message)
+    public constructor(message: String?) : super(message)
 
-    constructor(message: String?, cause: Throwable?) : super(message, cause)
+    public constructor(message: String?, cause: Throwable?) : super(message, cause)
 
     /**
      * The request ID that was returned by the called service
      */
-    open var requestId: String = ""
+    public open var requestId: String = ""
 
     /**
      * Returns the error code associated with the response
      */
-    open var errorCode: String = ""
+    public open var errorCode: String = ""
 }
 
 /**
  * Base class for all exceptions originating from the AWS runtime
  */
-open class ClientException : ClientException {
+public open class ClientException : ClientException {
 
-    constructor() : super()
+    public constructor() : super()
 
-    constructor(message: String?) : super(message)
+    public constructor(message: String?) : super(message)
 
-    constructor(message: String?, cause: Throwable?) : super(message, cause)
+    public constructor(message: String?, cause: Throwable?) : super(message, cause)
 }
 
 /**
  * An exception that is thrown when the returned (error) response is not known by this version of the SDK
  * (i.e. the modeled error code is not known)
  */
-class UnknownServiceException : AwsServiceException {
-    constructor() : super()
+public class UnknownServiceErrorException : AwsServiceException {
 
-    constructor(message: String?) : super(message)
+    public constructor() : super()
 
-    constructor(message: String?, cause: Throwable?) : super(message, cause)
+    public constructor(message: String?) : super(message)
+
+    public constructor(message: String?, cause: Throwable?) : super(message, cause)
 }
