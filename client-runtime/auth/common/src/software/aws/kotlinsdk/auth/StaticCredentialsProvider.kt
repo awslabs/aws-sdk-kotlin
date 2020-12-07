12797ea2d722f883ee987577d8f8d67d00228329
Aaron J Todd <todaaron@amazon.com>
2020-12-07 08:57:02 -0500

    refactor(rt): simplify StaticProvider construction

--- client-runtime/auth/common/src/software/aws/kotlinsdk/auth/StaticCredentialsProvider.kt
+++ client-runtime/auth/common/src/software/aws/kotlinsdk/auth/StaticCredentialsProvider.kt
@@ -7,8 +7,10 @@ package software.aws.kotlinsdk.auth
 
 /**
  * A credentials provider for a fixed set of credentials
+ *
+ * @param credentials The set of static credentials this provider will return
  */
-public class StaticCredentialsProvider private constructor(private val credentials: Credentials) : CredentialsProvider {
+public class StaticCredentialsProvider public constructor(private val credentials: Credentials) : CredentialsProvider {
 
     private constructor(builder: Builder) : this(Credentials(builder.accessKeyId!!, builder.secretAccessKey!!, builder.sessionToken))
 
@@ -20,12 +22,7 @@ public class StaticCredentialsProvider private constructor(private val credentia
         /**
          * Construct a new [StaticCredentialsProvider] using the given [block]
          */
-        public fun build(block: Builder.() -> Unit): StaticCredentialsProvider = Builder().apply(block).build()
-
-        /**
-         * Construct a new [StaticCredentialsProvider] from a set of credentials
-         */
-        public fun fromCredentials(credentials: Credentials): StaticCredentialsProvider = StaticCredentialsProvider(credentials)
+        public operator fun invoke(block: Builder.() -> Unit): StaticCredentialsProvider = Builder().apply(block).build()
     }
 
     public class Builder {
