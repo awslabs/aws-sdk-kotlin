e6948400998bb03cb52454f814c89a8c8e74d27c
Aaron J Todd <todaaron@amazon.com>
2020-12-16 09:48:36 -0500

    chore: add test rollup task for KMP and add to ci tasks

--- client-runtime/build.gradle.kts
+++ client-runtime/build.gradle.kts
@@ -91,3 +91,25 @@ subprojects {
     apply(from = rootProject.file("gradle/publish.gradle"))
 }
 
+task<org.jetbrains.kotlin.gradle.testing.internal.KotlinTestReport>("rootAllTest"){
+    destinationDir = File(project.buildDir, "reports/tests/rootAllTest")
+    val rootAllTest = this
+    subprojects {
+        val proj = this
+        afterEvaluate {
+            if (tasks.findByName("allTests") != null) {
+                val provider = tasks.named("allTests")
+                val allTestsTaskProvider = provider as TaskProvider<org.jetbrains.kotlin.gradle.testing.internal.KotlinTestReport>
+                rootAllTest.addChild(allTestsTaskProvider)
+                rootAllTest.dependsOn(allTestsTaskProvider)
+            }
+        }
+    }
+
+    beforeEvaluate {
+        project.gradle.taskGraph.whenReady {
+            rootAllTest.maybeOverrideReporting(this)
+        }
+    }
+}
+
