47d4b7e3fdba0a85b57cd3b8ccf1f19385c60f89
Ken Gilmer <kggilmer@amazon.com>
2020-12-30 12:47:15 -0800

    Cleanup from PR feedback

--- client-runtime/protocols/rest-json/common/src/software/aws/kotlinsdk/restjson/AwsJsonTargetHeader.kt
+++ client-runtime/protocols/rest-json/common/src/software/aws/kotlinsdk/restjson/AwsJsonTargetHeader.kt
@@ -30,17 +30,17 @@ public class AwsJsonTargetHeader() : Feature {
 
     override fun install(client: SdkHttpClient) {
         client.requestPipeline.intercept(HttpRequestPipeline.Initialize) {
-            val serviceName = this.context.executionContext[SdkClientOption.ServiceName]
-            val operationName = this.context.executionContext[SdkClientOption.OperationName]
+            val serviceName = context.executionContext[SdkClientOption.ServiceName]
+            val operationName = context.executionContext[SdkClientOption.OperationName]
 
             // TODO - associate this operation a link to future awsJson1_0 spec.
-            this.subject.headers.append("X-Amz-Target", "$serviceName.$operationName")
+            subject.headers.append("X-Amz-Target", "$serviceName.$operationName")
 
             // TODO - in the case of awsJson requests without inputs, there is no serializer associated w/ the operation
             //  As-is the serializer is responsible for populating the Content-Type header.  This may change
             //  when synthetic inputs are added such that all operations have inputs.  Readdress this issue after that work
             //  is complete.
-            if (!this.subject.headers.contains("Content-Type")) {
+            if (!subject.headers.contains("Content-Type")) {
                 subject.headers.append("Content-Type", "application/x-amz-json-1.0")
             }
         }
