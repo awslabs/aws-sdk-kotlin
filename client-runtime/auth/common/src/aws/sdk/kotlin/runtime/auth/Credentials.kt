77a84bf841b20bd465d09154512aaab6d8178e46
Aaron J Todd <todaaron@amazon.com>
2021-01-21 09:36:23 -0500

    fix: insert static credentials for generated protocol tests as necessary

--- client-runtime/auth/common/src/aws/sdk/kotlin/runtime/auth/Credentials.kt
+++ client-runtime/auth/common/src/aws/sdk/kotlin/runtime/auth/Credentials.kt
@@ -8,4 +8,4 @@ package aws.sdk.kotlin.runtime.auth
 /**
  * Represents a set of AWS credentials
  */
-public data class Credentials(val accessKeyId: String, val secretAccessKey: String, val sessionToken: String?)
+public data class Credentials(val accessKeyId: String, val secretAccessKey: String, val sessionToken: String? = null)
