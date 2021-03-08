bf82159cd68b7d93cb95dea5d536f8f1cce42a35
Aaron J Todd <todaaron@amazon.com>
2021-03-08 13:55:13 -0500

    add EndpointResolver to generated client config

---
+++ client-runtime/aws-client-rt/common/src/aws/sdk/kotlin/runtime/endpoint/StaticEndpointResolver.kt
@@ -0,0 +1,15 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0.
+ */
+
+package aws.sdk.kotlin.runtime.endpoint
+
+/**
+ * An [EndpointResolver] that returns a static endpoint
+ */
+public class StaticEndpointResolver(private val endpoint: Endpoint) : EndpointResolver {
+    override suspend fun resolve(service: String, region: String): Endpoint {
+        return endpoint
+    }
+}
