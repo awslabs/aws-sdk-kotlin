4369a6d81cee01fe7aa7d455beca259f235831d3
Ken Gilmer <65979407+kggilmer@users.noreply.github.com>
2021-05-03 10:05:50 -0700

    fix(codegen): http error registration (#118)

--- codegen/smithy-aws-kotlin-codegen/src/main/kotlin/aws/sdk/kotlin/codegen/protocols/xml/RestXmlErrorMiddleware.kt
+++ codegen/smithy-aws-kotlin-codegen/src/main/kotlin/aws/sdk/kotlin/codegen/protocols/xml/RestXmlErrorMiddleware.kt
@@ -9,9 +9,11 @@ import aws.sdk.kotlin.codegen.AwsKotlinDependency
 import aws.sdk.kotlin.codegen.protocols.middleware.ModeledExceptionsMiddleware
 import software.amazon.smithy.kotlin.codegen.core.KotlinWriter
 import software.amazon.smithy.kotlin.codegen.core.addImport
+import software.amazon.smithy.kotlin.codegen.model.expectTrait
 import software.amazon.smithy.kotlin.codegen.model.getTrait
 import software.amazon.smithy.kotlin.codegen.rendering.protocol.HttpBindingResolver
 import software.amazon.smithy.kotlin.codegen.rendering.protocol.ProtocolGenerator
+import software.amazon.smithy.model.traits.ErrorTrait
 import software.amazon.smithy.model.traits.HttpErrorTrait
 
 class RestXmlErrorMiddleware(
@@ -32,9 +34,11 @@ class RestXmlErrorMiddleware(
             val code = errShape.id.name
             val symbol = ctx.symbolProvider.toSymbol(errShape)
             val deserializerName = "${symbol.name}Deserializer"
-            errShape.getTrait<HttpErrorTrait>()?.code?.let { httpStatusCode ->
-                writer.write("register(code = #S, deserializer = $deserializerName(), httpStatusCode = $httpStatusCode)", code)
-            }
+            // If model specifies error code use it otherwise default to 400 (client) or 500 (server)
+            // See https://awslabs.github.io/smithy/1.0/spec/core/http-traits.html#httperror-trait
+            val defaultCode = if (errShape.expectTrait<ErrorTrait>().isClientError) 400 else 500
+            val httpStatusCode = errShape.getTrait<HttpErrorTrait>()?.code ?: defaultCode
+            writer.write("register(code = #S, deserializer = $deserializerName(), httpStatusCode = $httpStatusCode)", code)
         }
     }
 }
