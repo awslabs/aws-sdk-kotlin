135a912571effaaec08c679343702d8cb6c4da91
Aaron Todd <aajtodd@users.noreply.github.com>
2021-05-04 09:02:57 -0400

    fix: explicitly set jvm target compatibility (#103)
    Explicilty set the java source/target compatibility so that the outgoing
    gradle variant metadata isn't inferred from the JVM version gradle is
    running with.
    
    see: https://docs.gradle.org/current/userguide/variant_attributes.html#sub:jvm_default_attributes
    
    fixes: https://github.com/awslabs/smithy-kotlin/issues/258

--- services/build.gradle.kts
+++ services/build.gradle.kts
@@ -5,7 +5,7 @@
  */
 plugins {
     kotlin("jvm")
-    `maven`
+    maven
     `maven-publish`
     id("org.jetbrains.dokka")
 }
@@ -43,6 +43,18 @@ subprojects {
         }
     }
 
+
+    // this is the default but it's better to be explicit (e.g. it may change in Kotlin 1.5)
+    tasks.compileKotlin {
+        kotlinOptions.jvmTarget = "1.6"
+    }
+    tasks.compileTestKotlin {
+        kotlinOptions.jvmTarget = "1.6"
+    }
+
+    // FIXME - we can remove this when we implement generated services as multiplatform.
+    setOutgoingVariantMetadata()
+
     val sourcesJar by tasks.creating(Jar::class) {
         group = "publishing"
         description = "Assembles Kotlin sources jar"
@@ -64,4 +76,14 @@ subprojects {
     }
 
     apply(from = rootProject.file("gradle/publish.gradle"))
+}
+
+
+// fixes outgoing variant metadata: https://github.com/awslabs/smithy-kotlin/issues/258
+fun Project.setOutgoingVariantMetadata() {
+    tasks.withType<JavaCompile>() {
+        val javaVersion = JavaVersion.VERSION_1_8.toString()
+        sourceCompatibility = javaVersion
+        targetCompatibility = javaVersion
+    }
 }
