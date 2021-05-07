af5fac41fdf39732daed432e110ca7f9ad78b2c3
Ken Gilmer <kggilmer@amazon.com>
2021-05-07 14:53:12 -0700

    Updates based on PR feedback.

--- examples/s3-media-ingestion/src/main/kotlin/Main.kt
+++ examples/s3-media-ingestion/src/main/kotlin/Main.kt
@@ -3,62 +3,128 @@
  * SPDX-License-Identifier: Apache-2.0.
  */
 import aws.sdk.kotlin.services.s3.S3Client
-import aws.sdk.kotlin.services.s3.model.ListBucketsRequest
-import aws.sdk.kotlin.services.s3.model.ListObjectsRequest
-import aws.sdk.kotlin.services.s3.model.PutObjectRequest
+import aws.sdk.kotlin.services.s3.model.*
 import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.runBlocking
 import software.aws.clientrt.content.ByteStream
 import software.aws.clientrt.content.fromFile
+import software.aws.clientrt.content.writeToFile
 import java.io.File
 import java.nio.file.Files
 
-const val bucketName = "s3-media-ingestion-example"
+/**
+ * This program reads media files from a specified directory and uploads media files to S3
+ *
+ * This is purely an example.  Any file with the extension `.avi` will be processed.  To test
+ * create a text file and name it such that it matches the [filenameMetadataRegex] regex, ex:
+ * `title_2000.avi`.
+ *
+ * When running the sample adjust the following path constants as needed for your local environment.
+ */
+const val bucketName = "s3-media-ingestion-example2"
 const val ingestionDirPath = "/tmp/media-in"
 const val completedDirPath = "/tmp/media-processed"
 const val failedDirPath = "/tmp/media-failed"
+const val downloadDirPath = "/tmp/media-failed"
 
 // media metadata is extracted from filename: <title>_<year>.avi
 val filenameMetadataRegex = "([\\w\\s]+)_([\\d]+).avi".toRegex()
 
-/**
- * This program reads media files from a specified directory and uploads media files to S3
- */
-fun main() = runBlocking {
+fun main(): Unit = runBlocking {
     val client = S3Client { region = "us-east-2" }
 
     try {
-        validateS3(client)
-        listOf(completedDirPath, failedDirPath).forEach { validateDirectory(it) }
+        // Setup
+        client.ensureBucketExists(bucketName)
+        listOf(completedDirPath, failedDirPath, downloadDirPath).forEach { validateDirectory(it) }
         val ingestionDir = validateDirectory(ingestionDirPath)
 
+        // Upload files
         val uploadResults = ingestionDir
-            .walk().asFlow()
+            .walk()
+            .asFlow()
             .mapNotNull(::mediaMetadataExtractor)
-            .map { mediaMetadata ->
-                uploadToS3(client, mediaMetadata)
-            }
+            .map { mediaMetadata -> client.uploadToS3(mediaMetadata) }
             .toList()
 
         moveFiles(uploadResults)
 
+        // Print results of operation
         val (successes, failures) = uploadResults.partition { it is Success }
         when (failures.isEmpty()) {
             true -> println("Media uploaded successfully: $successes")
             false -> println("Successfully uploaded: $successes \nFailed to upload: $failures")
+        }
+
+        // Download files to verify
+        client.listObjects(ListObjectsRequest { bucket = bucketName }).contents?.forEach { obj ->
+            client.getObject(GetObjectRequest { key = obj.key; bucket = bucketName }) { response ->
+                response.body?.writeToFile(File(downloadDirPath, obj.key!!))
+            }
         }
     } finally {
         client.close()
     }
 }
 
-// Check for valid S3 configuration based on account
-suspend fun validateS3(s3Client: S3Client) {
-    val listBucketResponse = s3Client.listBuckets(ListBucketsRequest { })
-    check(listBucketResponse.buckets?.any { it.name == bucketName } ?: false) { "Bucket $bucketName does not exist" }
+/** Check for valid S3 configuration based on account */
+suspend fun S3Client.ensureBucketExists(bucketName: String) {
+    if (!bucketExists(bucketName)) {
+        createBucket(
+            CreateBucketRequest {
+                bucket = bucketName
+                createBucketConfiguration {
+                    locationConstraint = BucketLocationConstraint.UsEast2
+                }
+            }
+        )
+    }
 }
 
-// Move files to directories based on upload results
+/** Upload to S3 if file not already uploaded */
+suspend fun S3Client.uploadToS3(mediaMetadata: MediaMetadata): UploadResult {
+    if (keyExists(bucketName, mediaMetadata.s3KeyName))
+        return FileExistsError("${mediaMetadata.s3KeyName} already uploaded.", mediaMetadata)
+
+    return try {
+        putObject(
+            PutObjectRequest {
+                bucket = bucketName
+                key = mediaMetadata.s3KeyName
+                body = ByteStream.fromFile(mediaMetadata.file)
+                metadata = mediaMetadata.toMap()
+            }
+        )
+        Success("$bucketName/${mediaMetadata.s3KeyName}", mediaMetadata)
+    } catch (e: Exception) { // Checking Service Exception coming in future release
+        UploadError(e, mediaMetadata)
+    }
+}
+
+/** Determine if a object exists in a bucket */
+suspend fun S3Client.keyExists(s3bucket: String, s3key: String) =
+    try {
+        headObject(
+            HeadObjectRequest {
+                bucket = s3bucket
+                key = s3key
+            }
+        )
+        true
+    } catch (e: Exception) { // Checking Service Exception coming in future release
+        false
+    }
+
+/** Determine if a object exists in a bucket */
+suspend fun S3Client.bucketExists(s3bucket: String) =
+    try {
+        headBucket(HeadBucketRequest { bucket = s3bucket })
+        true
+    } catch (e: Exception) { // Checking Service Exception coming in future release
+        false
+    }
+
+/** Move files to directories based on upload results */
 fun moveFiles(uploadResults: List<UploadResult>) =
     uploadResults
         .map { uploadResult -> uploadResult.mediaMetadata.file.toPath() to (uploadResult is Success) }
@@ -72,30 +138,7 @@ fun moveFiles(uploadResults: List<UploadResult>) =
 sealed class UploadResult { abstract val mediaMetadata: MediaMetadata }
 data class Success(val location: String, override val mediaMetadata: MediaMetadata) : UploadResult()
 data class UploadError(val error: Throwable, override val mediaMetadata: MediaMetadata) : UploadResult()
-data class Failure(val reason: String, override val mediaMetadata: MediaMetadata) : UploadResult()
-
-// Upload to S3 if file not already uploaded
-suspend fun uploadToS3(s3Client: S3Client, mediaMetadata: MediaMetadata): UploadResult {
-    val existsInS3 = s3Client
-        .listObjects(ListObjectsRequest { bucket = bucketName })
-        .contents?.any { it.key == mediaMetadata.s3KeyName } ?: false
-
-    if (existsInS3) return Failure("${mediaMetadata.s3KeyName} already uploaded.", mediaMetadata)
-
-    return try {
-        s3Client.putObject(
-            PutObjectRequest {
-                bucket = bucketName
-                key = mediaMetadata.s3KeyName
-                body = ByteStream.fromFile(mediaMetadata.file)
-                metadata = mediaMetadata.toMap()
-            }
-        )
-        Success("$bucketName/${mediaMetadata.s3KeyName}", mediaMetadata)
-    } catch (e: Exception) {
-        UploadError(e, mediaMetadata)
-    }
-}
+data class FileExistsError(val reason: String, override val mediaMetadata: MediaMetadata) : UploadResult()
 
 // Classes, properties, and functions for media metadata
 data class MediaMetadata(val title: String, val year: Int, val file: File)
@@ -110,7 +153,7 @@ fun mediaMetadataExtractor(file: File): MediaMetadata? {
     return MediaMetadata(title, year.toInt(), file)
 }
 
-// Validate file path and optionally create directory
+/** Validate file path and optionally create directory */
 fun validateDirectory(dirPath: String): File {
     val dir = File(dirPath)
 
