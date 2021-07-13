5d5e0df368a0a11739188406554c5184aeed0283
Ken Gilmer <kggilmer@amazon.com>
2021-07-12 17:36:50 -0700

    cleanup

--- aws-runtime/auth/common/src/aws/sdk/kotlin/runtime/auth/Presigner.kt
+++ aws-runtime/auth/common/src/aws/sdk/kotlin/runtime/auth/Presigner.kt
@@ -11,6 +11,9 @@ import aws.smithy.kotlin.runtime.http.HeadersBuilder
 import aws.smithy.kotlin.runtime.http.HttpMethod
 import aws.smithy.kotlin.runtime.util.InternalApi
 
+/**
+ * The service configuration details for a presigned request
+ */
 interface ServicePresignConfig {
     public val region: String
     public val serviceName: String
@@ -18,10 +21,16 @@ interface ServicePresignConfig {
     public val credentialsProvider: CredentialsProvider
 }
 
+/**
+ * Where the signature is placed in the presigned request
+ */
 public enum class SigningLocation {
     HEADER, QUERY_STRING
 }
 
+/**
+ * Configuration of a presigned request
+ */
 public data class PresignedRequestConfig(
     public val signedHeaderKeys: Set<String>,
     public val method: HttpMethod,
@@ -32,14 +41,23 @@ public data class PresignedRequestConfig(
     public val additionalHeaders: Headers
 )
 
+/**
+ * Properties of an HTTP request that has been presigned
+ */
 public data class PresignedRequest(
     val url: String,
     val headers: Headers,
     val method: HttpMethod
 )
 
+/**
+ * Generate a presigned request given the service and operation configurations.
+ * @param serviceConfig The service configuration to use in signing the request
+ * @param requestConfig The presign configuration to use in signing the request
+ * @return a [PresignedRequest] that can be executed by any HTTP client within the specified duration.
+ */
 @InternalApi
-public suspend fun presignUrl(serviceConfig: ServicePresignConfig, requestConfig: PresignedRequestConfig) : PresignedRequest {
+public suspend fun createPresignedRequest(serviceConfig: ServicePresignConfig, requestConfig: PresignedRequestConfig) : PresignedRequest {
     val crtCredentials = serviceConfig.credentialsProvider?.getCredentials()?.toCrt() ?: error("Must specify credentialsProvider.")
     val signingConfig: AwsSigningConfig = AwsSigningConfig.build {
         region = serviceConfig.region
@@ -54,7 +72,7 @@ public suspend fun presignUrl(serviceConfig: ServicePresignConfig, requestConfig
     val endpoint = serviceConfig.endpointResolver.resolve(serviceConfig.serviceName, serviceConfig.region!!)
     val url = "${endpoint.protocol}://${endpoint.hostname}${requestConfig.path}"
     val headers = aws.sdk.kotlin.crt.http.Headers.build {
-        append("host", endpoint.hostname)
+        append("Host", endpoint.hostname)
         appendAll(requestConfig.additionalHeaders.toCrtHeaders())
     }
     val request = HttpRequest(
@@ -67,6 +85,7 @@ public suspend fun presignUrl(serviceConfig: ServicePresignConfig, requestConfig
     return PresignedRequest(signedRequest.encodedPath, signedRequest.headers.toSdkHeaders(), HttpMethod.parse(signedRequest.method))
 }
 
+// Convert CRT header type to SDK header type
 private fun aws.sdk.kotlin.crt.http.Headers.toSdkHeaders(): Headers {
     val hdrs =  HeadersBuilder()
 
@@ -77,6 +96,7 @@ private fun aws.sdk.kotlin.crt.http.Headers.toSdkHeaders(): Headers {
     return hdrs.build()
 }
 
+// Convert SDK header type to CRT header type
 private fun Headers.toCrtHeaders(): aws.sdk.kotlin.crt.http.Headers {
     val hdrs =  aws.sdk.kotlin.crt.http.HeadersBuilder()
 
