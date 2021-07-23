b4973f659c104665c6b7972e1daf8f77edf277fb
Ken Gilmer <kggilmer@amazon.com>
2021-07-23 14:45:22 -0700

    Move polly presigner into handwritten customization

---
+++ services/polly/common/test/aws/sdk/kotlin/services/polly/PollyTest.kt
@@ -0,0 +1,43 @@
+package aws.sdk.kotlin.services.polly
+
+import aws.sdk.kotlin.runtime.testing.runSuspendTest
+import aws.sdk.kotlin.services.polly.model.OutputFormat
+import aws.sdk.kotlin.services.polly.model.SynthesizeSpeechRequest
+import aws.sdk.kotlin.services.polly.model.VoiceId
+import aws.sdk.kotlin.services.polly.transform.SynthesizeSpeechOperationSerializer
+import aws.smithy.kotlin.runtime.client.ExecutionContext
+import aws.smithy.kotlin.runtime.http.HttpMethod
+import aws.smithy.kotlin.runtime.http.Url
+import kotlinx.coroutines.runBlocking
+import org.junit.jupiter.api.Test
+import java.net.URL
+import javax.net.ssl.HttpsURLConnection
+import kotlin.test.assertEquals
+import kotlin.test.assertTrue
+
+class PollyPresignerTest {
+
+    @Test
+    fun itProducesExpectedUrlComponents() = runSuspendTest {
+        val request = SynthesizeSpeechRequest {
+            voiceId = VoiceId.Salli
+            outputFormat = OutputFormat.Pcm
+            text = "hello world"
+        }
+
+        val clientConfig = PollyPresignConfig {
+            region = "us-east-2"
+        }
+
+        val presignedRequest = request.presign(clientConfig, 10u)
+
+        assertEquals(HttpMethod.GET, presignedRequest.method)
+        assertTrue(presignedRequest.headers.entries().size == 1)
+        assertEquals("polly.us-east-2.amazonaws.com", presignedRequest.headers["Host"])
+        val parsedUrl = Url.parse(presignedRequest.url)
+        assertEquals("polly.us-east-2.amazonaws.com", parsedUrl.host)
+        assertEquals("/v1/speech", parsedUrl.path)
+        val expectedQueryParameters = setOf("OutputFormat", "Text", "VoiceId", "X-Amz-Algorithm", "X-Amz-Credential", "X-Amz-Date", "X-Amz-SignedHeaders", "X-Amz-Expires", "X-Amz-Signature")
+        assertEquals(expectedQueryParameters, parsedUrl.parameters.entries().map { it.key }.toSet())
+    }
+}
