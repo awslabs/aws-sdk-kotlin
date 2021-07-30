adae24b7c3400529e445d42fff98465893b4544b
Aaron J Todd <todaaron@amazon.com>
2021-07-30 11:29:14 -0400

    fix: register signing middleware only if supported by operation

--- codegen/smithy-aws-kotlin-codegen/src/main/kotlin/aws/sdk/kotlin/codegen/protocols/middleware/AwsSignatureVersion4.kt
+++ codegen/smithy-aws-kotlin-codegen/src/main/kotlin/aws/sdk/kotlin/codegen/protocols/middleware/AwsSignatureVersion4.kt
@@ -10,8 +10,10 @@ import aws.sdk.kotlin.codegen.AwsRuntimeTypes
 import software.amazon.smithy.aws.traits.auth.SigV4Trait
 import software.amazon.smithy.kotlin.codegen.core.KotlinWriter
 import software.amazon.smithy.kotlin.codegen.core.addImport
+import software.amazon.smithy.kotlin.codegen.model.expectShape
 import software.amazon.smithy.kotlin.codegen.model.expectTrait
 import software.amazon.smithy.kotlin.codegen.model.hasTrait
+import software.amazon.smithy.kotlin.codegen.rendering.protocol.ProtocolGenerator
 import software.amazon.smithy.kotlin.codegen.rendering.protocol.ProtocolMiddleware
 import software.amazon.smithy.model.Model
 import software.amazon.smithy.model.knowledge.ServiceIndex
@@ -24,11 +26,16 @@ import software.amazon.smithy.model.traits.OptionalAuthTrait
  * @param signingServiceName The credential scope service name to sign for
  * See the `name` property of: https://awslabs.github.io/smithy/1.0/spec/aws/aws-auth.html#aws-auth-sigv4-trait
  */
-open class AwsSignatureVersion4(val signingServiceName: String) : ProtocolMiddleware {
+open class AwsSignatureVersion4(private val signingServiceName: String) : ProtocolMiddleware {
     override val name: String = AwsRuntimeTypes.Auth.AwsSigV4SigningMiddleware.name
 
     init {
         require(signingServiceName.isNotEmpty()) { "signingServiceName must be specified" }
+    }
+
+    override fun isEnabledFor(ctx: ProtocolGenerator.GenerationContext, op: OperationShape): Boolean {
+        val service = ctx.model.expectShape<ServiceShape>(ctx.settings.service)
+        return hasSigV4AuthScheme(ctx.model, service, op)
     }
 
     override fun addImportsAndDependencies(writer: KotlinWriter) {
