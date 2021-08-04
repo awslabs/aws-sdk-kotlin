81a413507da06da74af9d2d5fb5827ea32d65801
Ian Smith Botsford <issmith@amazon.com>
2021-08-04 21:56:56 +0000

    fix: disable signing config for double encoding and path normalization for S3

--- codegen/smithy-aws-kotlin-codegen/src/main/kotlin/aws/sdk/kotlin/codegen/customization/s3/S3SigningConfig.kt
+++ codegen/smithy-aws-kotlin-codegen/src/main/kotlin/aws/sdk/kotlin/codegen/customization/s3/S3SigningConfig.kt
@@ -46,5 +46,9 @@ private class S3SigningMiddleware(signingServiceName: String) : AwsSignatureVers
         val sbht = AwsRuntimeTypes.Auth.AwsSignedBodyHeaderType
         writer.addImport(sbht)
         writer.write("signedBodyHeaderType = #T.X_AMZ_CONTENT_SHA256", sbht)
+
+        // https://github.com/awslabs/aws-sdk-kotlin/issues/200
+        writer.write("useDoubleUriEncode = false")
+        writer.write("normalizeUriPath = false")
     }
 }
