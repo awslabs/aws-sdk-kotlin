b4c12e62d399016423b0abe9fde00cd6158bc441
Ken Gilmer <65979407+kggilmer@users.noreply.github.com>
2021-08-24 13:44:22 -0700

    refactor(rt): Use existing type HttpRequest rather than specific PresignedRequest to represent presigned requests (#292)

--- codegen/smithy-aws-kotlin-codegen/src/main/kotlin/aws/sdk/kotlin/codegen/PresignerGenerator.kt
+++ codegen/smithy-aws-kotlin-codegen/src/main/kotlin/aws/sdk/kotlin/codegen/PresignerGenerator.kt
@@ -75,11 +75,11 @@ class PresignerGenerator : KotlinIntegration {
     // Symbols which should be imported
     private val presignerRuntimeSymbols = setOf(
         // smithy-kotlin types
+        RuntimeTypes.Http.Request.HttpRequest,
         RuntimeTypes.Core.ExecutionContext,
         // AWS types
         AwsRuntimeTypes.Auth.CredentialsProvider,
         AwsRuntimeTypes.Auth.DefaultChainCredentialsProvider,
-        AwsRuntimeTypes.Auth.PresignedRequest,
         AwsRuntimeTypes.Auth.PresignedRequestConfig,
         AwsRuntimeTypes.Auth.ServicePresignConfig,
         AwsRuntimeTypes.Auth.SigningLocation,
@@ -327,10 +327,10 @@ class PresignerGenerator : KotlinIntegration {
             write("Presign a [$requestTypeName] using a [$serviceClientTypeName].")
             write("@param serviceClient the client providing properties used to generate the presigned request.")
             write("@param durationSeconds the amount of time from signing for which the request is valid, with seconds granularity.")
-            write("@return The [PresignedRequest] that can be invoked within the specified time window.")
+            write("@return The [HttpRequest] that can be invoked within the specified time window.")
         }
         // FIXME ~ Replace or add additional function, swap ULong type for kotlin.time.Duration when type becomes stable
-        writer.withBlock("suspend fun $requestTypeName.presign(serviceClient: $serviceClientTypeName, durationSeconds: ULong): PresignedRequest {", "}\n") {
+        writer.withBlock("suspend fun $requestTypeName.presign(serviceClient: $serviceClientTypeName, durationSeconds: ULong): HttpRequest {", "}\n") {
             withBlock("val serviceClientConfig = $presignConfigTypeName {", "}") {
                 write("credentialsProvider = serviceClient.config.credentialsProvider ?: DefaultChainCredentialsProvider()")
                 write("endpointResolver = serviceClient.config.endpointResolver ?: DefaultEndpointResolver()")
@@ -345,10 +345,10 @@ class PresignerGenerator : KotlinIntegration {
             write("Presign a [$requestTypeName] using a [ServicePresignConfig].")
             write("@param serviceClientConfig the client configuration used to generate the presigned request")
             write("@param durationSeconds the amount of time from signing for which the request is valid, with seconds granularity.")
-            write("@return The [PresignedRequest] that can be invoked within the specified time window.")
+            write("@return The [HttpRequest] that can be invoked within the specified time window.")
         }
         // FIXME ~ Replace or add additional function, swap ULong type for kotlin.time.Duration when type becomes stable
-        writer.withBlock("suspend fun $requestTypeName.presign(serviceClientConfig: ServicePresignConfig, durationSeconds: ULong): PresignedRequest {", "}\n") {
+        writer.withBlock("suspend fun $requestTypeName.presign(serviceClientConfig: ServicePresignConfig, durationSeconds: ULong): HttpRequest {", "}\n") {
             write("return createPresignedRequest(serviceClientConfig, $requestConfigFnName(this, durationSeconds))")
         }
     }
