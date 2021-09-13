4d290a58ce5ecf8aae6240e79965c9bd4c5d0148
Ken <58150075+kmbotts@users.noreply.github.com>
2021-09-13 09:43:04 -0400

    Profile Auth Support JVM system property and Environment Variables #297 (#312)

---
+++ aws-runtime/auth/jvm/src/aws/sdk/kotlin/runtime/auth/ProfileCredentialsProviderJvm.kt
@@ -0,0 +1,21 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0.
+ */
+
+package aws.sdk.kotlin.runtime.auth
+
+import aws.sdk.kotlin.runtime.AwsSdkSetting
+
+/**
+ * Attempts to load profile name using the following priority,
+ * if neither are available, profileName will remain blank.
+ * 1. JVM System Property
+ * 2. Environment Variable
+ */
+internal actual fun loadProfileName(): String? {
+    val profileSysPropOverride = System.getProperty(AwsSdkSetting.AwsProfile.jvmProperty, "")
+    return profileSysPropOverride.ifEmpty {
+        System.getenv().getOrDefault(AwsSdkSetting.AwsProfile.environmentVariable, null)
+    }
+}
