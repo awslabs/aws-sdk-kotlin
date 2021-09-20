d533c6daeb72446bd4e62c4ce05216fb36b8d196
Aaron J Todd <todaaron@amazon.com>
2021-09-20 09:11:41 -0400

    cleanup kdoc

--- aws-runtime/auth/common/src/aws/sdk/kotlin/runtime/auth/signing/AwsSigning.kt
+++ aws-runtime/auth/common/src/aws/sdk/kotlin/runtime/auth/signing/AwsSigning.kt
@@ -13,6 +13,17 @@ import aws.smithy.kotlin.runtime.http.request.toBuilder
 
 /**
  * Container for signed output and signature
+ *
+ * @property output The signed output type (e.g. HttpRequest)
+ * @property signature The resulting signature. Depending on the requested signature type and algorithm,
+ * this value will be in one of the following formats:
+ *
+ *   1. [AwsSignatureType.HTTP_REQUEST_VIA_HEADERS] - hex encoding of the binary signature value
+ *   2. [AwsSignatureType.HTTP_REQUEST_VIA_QUERY_PARAMS] - hex encoding of the binary signature value
+ *   3. [AwsSignatureType.HTTP_REQUEST_CHUNK] (SIGV4) - hex encoding of the binary signature value
+ *   4. [AwsSignatureType.HTTP_REQUEST_CHUNK] (SIGV4_ASYMMETRIC) - '*'-padded hex encoding of the binary signature value
+ *   5. [AwsSignatureType.HTTP_REQUEST_EVENT] - binary signature value (NYI)
+ *
  */
 public data class SigningResult<T>(val output: T, val signature: ByteArray) {
     override fun equals(other: Any?): Boolean {
@@ -36,7 +47,10 @@ public data class SigningResult<T>(val output: T, val signature: ByteArray) {
 
 /**
  * Sign [HttpRequest] using the given signing [config]
- * @return signing result
+ *
+ * @param request the HTTP request to sign
+ * @param config the signing configuration to use
+ * @return the signing result
  */
 public suspend fun sign(request: HttpRequest, config: AwsSigningConfig): SigningResult<HttpRequest> {
     val crtRequest = request.toSignableCrtRequest()
