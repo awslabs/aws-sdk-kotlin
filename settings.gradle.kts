9fae52b39f0c113f7010f2e4d57a180c42025d57
Aaron J Todd <todaaron@amazon.com>
2021-10-11 16:06:38 -0400

    event-stream -> aws-event-stream

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -38,7 +38,7 @@ include(":aws-runtime:protocols:http")
 include(":aws-runtime:http-client-engine-crt")
 include(":aws-runtime:protocols:aws-json-protocols")
 include(":aws-runtime:protocols:aws-xml-protocols")
-include(":aws-runtime:protocols:event-stream")
+include(":aws-runtime:protocols:aws-event-stream")
 include(":aws-runtime:crt-util")
 
 // generated services
