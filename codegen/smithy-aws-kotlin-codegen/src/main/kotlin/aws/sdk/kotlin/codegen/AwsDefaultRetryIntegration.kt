0bed43b89a6bcdd23a9b49004aa90d536f7f40c1
Ian Botsford <83236726+ianbotsf@users.noreply.github.com>
2021-10-12 13:45:13 -0700

    feat: add codegen wrappers for retries (#331)

---
+++ codegen/smithy-aws-kotlin-codegen/src/main/kotlin/aws/sdk/kotlin/codegen/AwsDefaultRetryIntegration.kt
@@ -0,0 +1,39 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0.
+ */
+
+package aws.sdk.kotlin.codegen
+
+import software.amazon.smithy.kotlin.codegen.core.KotlinWriter
+import software.amazon.smithy.kotlin.codegen.core.RuntimeTypes
+import software.amazon.smithy.kotlin.codegen.integration.KotlinIntegration
+import software.amazon.smithy.kotlin.codegen.rendering.protocol.HttpFeatureMiddleware
+import software.amazon.smithy.kotlin.codegen.rendering.protocol.ProtocolGenerator
+import software.amazon.smithy.kotlin.codegen.rendering.protocol.ProtocolMiddleware
+import software.amazon.smithy.kotlin.codegen.rendering.protocol.replace
+import software.amazon.smithy.kotlin.codegen.retries.StandardRetryMiddleware
+
+/**
+ * Adds AWS-specific retry wrappers around operation invocations. This replaces
+ * [StandardRetryPolicy][aws.smithy.kotlin.runtime.retries.impl] with
+ * [AwsDefaultRetryPolicy][aws.sdk.kotlin.runtime.http.retries].
+ */
+class AwsDefaultRetryIntegration : KotlinIntegration {
+    override fun customizeMiddleware(
+        ctx: ProtocolGenerator.GenerationContext,
+        resolved: List<ProtocolMiddleware>
+    ): List<ProtocolMiddleware> = resolved.replace(middleware) { it.name == StandardRetryMiddleware.name }
+}
+
+private val middleware = object : HttpFeatureMiddleware() {
+    override val name: String = "RetryFeature"
+
+    override fun renderConfigure(writer: KotlinWriter) {
+        writer.addImport(RuntimeTypes.Http.Middlware.RetryFeature)
+        writer.addImport(AwsRuntimeTypes.Http.Retries.AwsDefaultRetryPolicy)
+
+        writer.write("strategy = config.retryStrategy")
+        writer.write("policy = AwsDefaultRetryPolicy")
+    }
+}
