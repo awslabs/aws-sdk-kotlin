d2351d280d041db3389566a85c1ec1fa254037f0
Aaron J Todd <todaaron@amazon.com>
2021-10-14 11:47:20 -0400

    update provider docs

--- aws-runtime/aws-config/common/src/aws/sdk/kotlin/runtime/auth/credentials/ImdsCredentialsProvider.kt
+++ aws-runtime/aws-config/common/src/aws/sdk/kotlin/runtime/auth/credentials/ImdsCredentialsProvider.kt
@@ -16,20 +16,20 @@ import aws.smithy.kotlin.runtime.util.Platform
 import aws.smithy.kotlin.runtime.util.PlatformEnvironProvider
 import aws.smithy.kotlin.runtime.util.asyncLazy
 
-private const val PROFILE_PATH: String = "/latest/meta-data/iam/security-credentials"
 private const val CREDENTIALS_BASE_PATH: String = "/latest/meta-data/iam/security-credentials"
 private const val CODE_ASSUME_ROLE_UNAUTHORIZED_ACCESS: String = "AssumeRoleUnauthorizedAccess"
 
 /**
- * [CredentialsProvider] that uses EC2 instance metadata service (IMDS) to provide credentials information
+ * [CredentialsProvider] that uses EC2 instance metadata service (IMDS) to provide credentials information.
+ * This provider requires that the EC2 instance has an [instance profile](https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/iam-roles-for-amazon-ec2.html#ec2-instance-profile)
+ * configured.
  *
- *  When retrieving credentials, a call must first be made to
- * `<IMDS_BASE_URL>/latest/meta-data/iam/security-credentials`. This returns the instance
- * profile used. If [profileOverride] is set, the initial call to retrieve the profile is skipped
- * and the provided value is used instead.
+ * See [EC2 IAM Roles](https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/iam-roles-for-amazon-ec2.html) for more
+ * information.
  *
- * @param profileOverride override the
- * [instance profile](https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/iam-roles-for-amazon-ec2.html#ec2-instance-profile) used for this provider
+ * @param profileOverride override the instance profile name. When retrieving credentials, a call must first be made to
+ * `<IMDS_BASE_URL>/latest/meta-data/iam/security-credentials`. This returns the instance profile used. If
+ * [profileOverride] is set, the initial call to retrieve the profile is skipped and the provided value is used instead.
  * @param client the IMDS client to use to resolve credentials information with
  * @param platformProvider the [PlatformEnvironProvider] instance
  */
@@ -84,7 +84,7 @@ public class ImdsCredentialsProvider(
 
     private suspend fun loadProfile(): String {
         return try {
-            client.value.get(PROFILE_PATH)
+            client.value.get(CREDENTIALS_BASE_PATH)
         } catch (ex: EC2MetadataError) {
             if (ex.statusCode == 404) {
                 logger.info { "received 404 from IMDS when loading profile information. Hint: This instance may not have an IAM role associated" }
