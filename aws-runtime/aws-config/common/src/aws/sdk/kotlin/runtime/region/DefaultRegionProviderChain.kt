0505e35e4efc262ceee8f82f745d3729ff7ea787
Aaron Todd <aajtodd@users.noreply.github.com>
2021-10-15 13:21:21 -0400

    feat(rt): ec2 imds region provider (#341)

--- aws-runtime/aws-config/common/src/aws/sdk/kotlin/runtime/region/DefaultRegionProviderChain.kt
+++ aws-runtime/aws-config/common/src/aws/sdk/kotlin/runtime/region/DefaultRegionProviderChain.kt
@@ -5,6 +5,8 @@
 
 package aws.sdk.kotlin.runtime.region
 
+import aws.smithy.kotlin.runtime.io.Closeable
+
 /**
  * [RegionProvider] that looks for region in this order:
  *  1. Check `aws.region` system property (JVM only)
@@ -12,4 +14,4 @@ package aws.sdk.kotlin.runtime.region
  *  3. Check the AWS config files/profile for region information
  *  4. If running on EC2, check the EC2 metadata service for region
  */
-public expect class DefaultRegionProviderChain public constructor() : RegionProvider
+public expect class DefaultRegionProviderChain public constructor() : RegionProvider, Closeable
