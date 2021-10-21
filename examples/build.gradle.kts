637f642f7d17470b8ee6af6e53f0e5ceb5767e9b
Aaron J Todd <todaaron@amazon.com>
2021-10-21 17:36:37 -0400

    chore: release 0.8.0-alpha

--- examples/build.gradle.kts
+++ examples/build.gradle.kts
@@ -2,9 +2,11 @@ plugins {
     kotlin("jvm") version "1.5.31"
 }
 
+val awsSdkKotlinVersion: String by project
+
 allprojects {
     group = "aws.sdk.kotlin.example"
-    version = "0.7.1-SNAPSHOT"
+    version = awsSdkKotlinVersion
 
     repositories {
         mavenLocal()
