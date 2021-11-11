2d4669ecbc93cd2f70f8252036116823ad12d373
Aaron J Todd <todaaron@amazon.com>
2021-11-11 11:01:48 -0500

    restore linux and macos targets

--- aws-runtime/http-client-engine-crt/jvm/test/aws/sdk/kotlin/runtime/http/engine/crt/AsyncStressTest.kt
+++ aws-runtime/http-client-engine-crt/jvm/test/aws/sdk/kotlin/runtime/http/engine/crt/AsyncStressTest.kt
@@ -27,10 +27,6 @@ import kotlin.time.Duration
 import kotlin.time.ExperimentalTime
 
 class AsyncStressTest : TestWithLocalServer() {
-//    init {
-//        System.setProperty("aws.crt.log.level", "Trace")
-//        System.setProperty("aws.crt.log.destination", "Stdout")
-//    }
 
     override val server = embeddedServer(CIO, serverPort) {
         routing {
