f665b309c26f3954dd79f06c9277c0b223e7b3de
Aaron J Todd <todaaron@amazon.com>
2021-12-01 15:08:56 -0500

    restore aws-config build for now

--- aws-runtime/aws-config/build.gradle.kts
+++ aws-runtime/aws-config/build.gradle.kts
@@ -1,12 +1,7 @@
-import aws.sdk.kotlin.gradle.codegen.dsl.smithyKotlinPlugin
-
 /*
  * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
  * SPDX-License-Identifier: Apache-2.0.
  */
-plugins {
-    id("aws.sdk.kotlin.codegen")
-}
 
 description = "Support for AWS configuration"
 extra["moduleName"] = "aws.sdk.kotlin.runtime.config"
@@ -36,17 +31,6 @@ kotlin {
                 implementation("aws.sdk.kotlin.crt:aws-crt-kotlin:$crtKotlinVersion")
                 implementation(project(":aws-runtime:crt-util"))
 
-
-
-                // additional dependencies required by generated sts provider
-                implementation("aws.smithy.kotlin:serde-form-url:$smithyKotlinVersion")
-                implementation("aws.smithy.kotlin:serde-xml:$smithyKotlinVersion")
-                implementation(project(":aws-runtime:protocols:aws-xml-protocols"))
-                implementation(project(":aws-runtime:aws-endpoint"))
-                implementation(project(":aws-runtime:aws-signing"))
-
-                // additional dependencies required by generated sso provider
-                implementation(project(":aws-runtime:protocols:aws-json-protocols"))
             }
         }
         commonTest {
@@ -69,113 +53,5 @@ kotlin {
             languageSettings.optIn("aws.smithy.kotlin.runtime.util.InternalApi")
             languageSettings.optIn("aws.sdk.kotlin.runtime.InternalSdkApi")
         }
-    }
-}
-
-fun awsModelFile(name: String): String =
-    rootProject.file("codegen/sdk/aws-models/$name").absolutePath
-
-codegen {
-    val basePackage = "aws.sdk.kotlin.runtime.auth.credentials.internal"
-
-    projections {
-        // generate an sts client
-        create("sts-credentials-provider") {
-            imports = listOf(
-                awsModelFile("sts.2011-06-15.json")
-            )
-
-            smithyKotlinPlugin {
-                serviceShapeId = "com.amazonaws.sts#AWSSecurityTokenServiceV20110615"
-                packageName = "${basePackage}.sts"
-                packageVersion = project.version.toString()
-                packageDescription = "Internal STS credentials provider"
-                sdkId = "STS"
-                buildSettings {
-                    generateDefaultBuildFiles = false
-                    generateFullProject = false
-                }
-            }
-
-            // TODO - could we add a trait such that we change visibility to `internal` or a build setting...?
-            transforms = listOf(
-                """
-            {
-                "name": "awsSdkKotlinIncludeOperations",
-                "args": {
-                    "operations": [
-                        "com.amazonaws.sts#AssumeRoleWithWebIdentity",
-                        "com.amazonaws.sts#AssumeRole"
-                    ]
-                }
-            }
-            """
-            )
-        }
-
-        // generate an sso client
-        create("sso-credentials-provider") {
-            imports = listOf(
-                awsModelFile("sso.2019-06-10.json")
-            )
-
-            val serviceShape = "com.amazonaws.sso#SWBPortalService"
-            smithyKotlinPlugin {
-                serviceShapeId = serviceShape
-                packageName = "${basePackage}.sso"
-                packageVersion = project.version.toString()
-                packageDescription = "Internal SSO credentials provider"
-                sdkId = "SSO"
-                buildSettings {
-                    generateDefaultBuildFiles = false
-                    generateFullProject = false
-                }
-            }
-
-            transforms = listOf(
-                """
-            {
-                "name": "awsSdkKotlinIncludeOperations",
-                "args": {
-                    "operations": [
-                        "com.amazonaws.sso#GetRoleCredentials"
-                    ]
-                }
-            }
-            """
-            )
-        }
-    }
-}
-
-/*
-NOTE: We need the following tasks to depend on codegen for gradle caching/up-to-date checks to work correctly:
-
-* `compileKotlinJvm` (Type=KotlinCompile)
-* `compileKotlinMetadata` (Type=KotlinCompileCommon)
-* `sourcesJar` and `jvmSourcesJar` (Type=org.gradle.jvm.tasks.Jar)
-*/
-val codegenTask = tasks.named("generateSmithyProjections")
-tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
-    dependsOn(codegenTask)
-}
-
-tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompileCommon> {
-    dependsOn(codegenTask)
-}
-
-tasks.withType<org.gradle.jvm.tasks.Jar> {
-    dependsOn(codegenTask)
-}
-
-
-codegen.projections.all {
-    // add this projected source dir to the common sourceSet
-    // NOTE - build.gradle.kts is still being generated, it's NOT used though
-    // TODO - we should probably either have a postProcessing spec or a plugin setting to not generate it to avoid confusion
-    val projectedSrcDir = projectionRootDir.resolve("src/main/kotlin")
-    kotlin.sourceSets.commonMain {
-        println("added $projectedSrcDir to common sourceSet")
-        kotlin.srcDir(projectedSrcDir)
     }
 }
