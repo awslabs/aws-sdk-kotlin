7042cf8c69b99cf616d8076086aa8a80c2225cf3
Aaron Todd <aajtodd@users.noreply.github.com>
2021-12-07 11:37:33 -0500

    chore: abstract codegen logic into a plugin (#457)

---
+++ codegen/smithy-aws-kotlin-codegen/src/main/kotlin/aws/sdk/kotlin/codegen/transforms/IncludeOperations.kt
@@ -0,0 +1,35 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0.
+ */
+
+package aws.sdk.kotlin.codegen.transforms
+
+import software.amazon.smithy.build.TransformContext
+import software.amazon.smithy.build.transforms.ConfigurableProjectionTransformer
+import software.amazon.smithy.model.Model
+import software.amazon.smithy.model.shapes.OperationShape
+
+/**
+ * A smithy build [transform](https://awslabs.github.io/smithy/1.0/guides/building-models/build-config.html#transforms)
+ * that filters out operations not included in the `operations` list of shape IDs
+ */
+class IncludeOperations : ConfigurableProjectionTransformer<IncludeOperations.Config>() {
+
+    class Config {
+        var operations: Set<String> = emptySet()
+    }
+
+    override fun getName(): String = "awsSdkKotlinIncludeOperations"
+    override fun getConfigType(): Class<Config> = Config::class.java
+
+    override fun transformWithConfig(context: TransformContext, config: Config): Model {
+        check(config.operations.isNotEmpty()) { "no operations provided to IncludeOperations transform!" }
+        return context.transformer.filterShapes(context.model) { shape ->
+            when (shape) {
+                is OperationShape -> shape.id.toString() in config.operations
+                else -> true
+            }
+        }
+    }
+}
---
+++ gradle/sdk-plugins/src/main/kotlin/aws/sdk/kotlin/gradle/codegen/CodegenPlugin.kt
@@ -0,0 +1,38 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0.
+ */
+
+package aws.sdk.kotlin.gradle.codegen
+
+import aws.sdk.kotlin.gradle.codegen.tasks.createSmithyCliConfiguration
+import aws.sdk.kotlin.gradle.codegen.tasks.registerCodegenTasks
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+const val CODEGEN_EXTENSION_NAME = "codegen"
+
+/**
+ * This plugin handles:
+ * - applying smithy-gradle-plugin to the project to generate code
+ * - providing a tasks to generate Kotlin sources from their respective smithy models.
+ */
+class CodegenPlugin : Plugin<Project> {
+    override fun apply(target: Project): Unit = target.run {
+        configurePlugins()
+        installExtension()
+        registerCodegenTasks()
+    }
+
+    private fun Project.configurePlugins() {
+        createSmithyCliConfiguration()
+        // unfortunately all of the tasks provided by smithy rely on the plugin extension, so it also needs applied
+        // see https://github.com/awslabs/smithy-gradle-plugin/issues/45
+        plugins.apply("software.amazon.smithy")
+        tasks.getByName("smithyBuildJar").enabled = false
+    }
+
+    private fun Project.installExtension(): CodegenExtension {
+        return extensions.create(CODEGEN_EXTENSION_NAME, CodegenExtension::class.java, project)
+    }
+}
