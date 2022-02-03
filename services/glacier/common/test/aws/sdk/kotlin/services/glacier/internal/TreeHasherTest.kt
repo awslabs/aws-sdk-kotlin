f58e627ec37b71068ba75d4fc94f319cde94ae94
Ken Gilmer <kggilmer@amazon.com>
2022-02-03 14:46:09 -0800

    lint fix

--- services/glacier/common/test/aws/sdk/kotlin/services/glacier/internal/TreeHasherTest.kt
+++ services/glacier/common/test/aws/sdk/kotlin/services/glacier/internal/TreeHasherTest.kt
@@ -15,12 +15,12 @@ import aws.smithy.kotlin.runtime.util.Sha256
 import aws.smithy.kotlin.runtime.util.encodeToHex
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.async
+import kotlinx.coroutines.test.runTest
 import kotlinx.coroutines.withTimeout
 import kotlin.test.Test
 import kotlin.test.assertContentEquals
 import kotlin.test.assertEquals
 import kotlin.test.fail
-import kotlinx.coroutines.test.runTest
 
 private const val megabyte = 1024 * 1024
 
