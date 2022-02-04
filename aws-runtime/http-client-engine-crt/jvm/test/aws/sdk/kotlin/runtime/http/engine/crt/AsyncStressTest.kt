9ce45d643bb351c165f84f897d33974b923e13ca
Ken Gilmer <kggilmer@amazon.com>
2022-02-04 10:20:45 -0800

    Add note regarding test regression

--- aws-runtime/http-client-engine-crt/jvm/test/aws/sdk/kotlin/runtime/http/engine/crt/AsyncStressTest.kt
+++ aws-runtime/http-client-engine-crt/jvm/test/aws/sdk/kotlin/runtime/http/engine/crt/AsyncStressTest.kt
@@ -72,7 +72,7 @@ class AsyncStressTest : TestWithLocalServer() {
         }
     }
 
-    @IgnoreWindows
+    @IgnoreWindows // FIXME - this test regressed with coroutines-test:1.6 in windows only, times out
     @Test
     fun testStreamNotConsumed() = runBlocking {
         // test that filling the stream window and not consuming the body stream still cleans up resources
