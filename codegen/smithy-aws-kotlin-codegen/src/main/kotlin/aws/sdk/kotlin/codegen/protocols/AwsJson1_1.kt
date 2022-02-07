919a9e1dd706b831324197e168714b5abcbb1022
Aaron J Todd <todaaron@amazon.com>
2022-02-07 11:03:58 -0500

    fix(codegen): ignore jsonName trait for awsJson protocols

--- codegen/smithy-aws-kotlin-codegen/src/main/kotlin/aws/sdk/kotlin/codegen/protocols/AwsJson1_1.kt
+++ codegen/smithy-aws-kotlin-codegen/src/main/kotlin/aws/sdk/kotlin/codegen/protocols/AwsJson1_1.kt
@@ -25,6 +25,7 @@ import software.amazon.smithy.model.shapes.ShapeId
  */
 class AwsJson1_1 : JsonHttpBindingProtocolGenerator() {
     override val protocol: ShapeId = AwsJson1_1Trait.ID
+    override val supportsJsonNameTrait: Boolean = false
 
     override fun getDefaultHttpMiddleware(ctx: ProtocolGenerator.GenerationContext): List<ProtocolMiddleware> {
         val httpMiddleware = super.getDefaultHttpMiddleware(ctx)
