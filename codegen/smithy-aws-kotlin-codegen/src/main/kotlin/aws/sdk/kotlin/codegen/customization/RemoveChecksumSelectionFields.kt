2ccdf4046c6928c9cf68cc3d0129535400488da6
Ian Botsford <83236726+ianbotsf@users.noreply.github.com>
2022-03-22 10:29:35 -0700

    fix: temporarily bypass httpchecksum traits until full flexible checksum support is available (#558)

---
+++ codegen/smithy-aws-kotlin-codegen/src/main/kotlin/aws/sdk/kotlin/codegen/customization/RemoveChecksumSelectionFields.kt
@@ -0,0 +1,62 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0.
+ */
+package aws.sdk.kotlin.codegen.customization
+
+import software.amazon.smithy.aws.traits.HttpChecksumTrait
+import software.amazon.smithy.kotlin.codegen.KotlinSettings
+import software.amazon.smithy.kotlin.codegen.integration.KotlinIntegration
+import software.amazon.smithy.kotlin.codegen.model.expectTrait
+import software.amazon.smithy.kotlin.codegen.model.hasTrait
+import software.amazon.smithy.kotlin.codegen.model.shapes
+import software.amazon.smithy.kotlin.codegen.utils.getOrNull
+import software.amazon.smithy.model.Model
+import software.amazon.smithy.model.shapes.MemberShape
+import software.amazon.smithy.model.shapes.OperationShape
+import software.amazon.smithy.model.transform.ModelTransformer
+import java.util.logging.Logger
+
+/**
+ * Temporary integration to remove flexible checksum fields from models.
+ * TODO https://github.com/awslabs/aws-sdk-kotlin/issues/557
+ */
+class RemoveChecksumSelectionFields : KotlinIntegration {
+    private val logger = Logger.getLogger(javaClass.name)
+
+    override val order: Byte = -127
+
+    override fun enabledForService(model: Model, settings: KotlinSettings): Boolean = model
+        .shapes<OperationShape>()
+        .any { it.hasTrait<HttpChecksumTrait>() }
+
+    override fun preprocessModel(model: Model, settings: KotlinSettings): Model {
+        val dropMembers = model
+            .shapes<OperationShape>()
+            .filter { it.hasTrait<HttpChecksumTrait>() }
+            .flatMap { op ->
+                val trait = op.expectTrait<HttpChecksumTrait>()
+
+                val requestAlgorithmMember = trait.requestAlgorithmMember.getOrNull()
+                val requestValidationModeMember = trait.requestValidationModeMember.getOrNull()
+
+                listOfNotNull(requestAlgorithmMember, requestValidationModeMember)
+                    .map { findInputMember(model, op, it) }
+            }
+            .toSet()
+
+        return ModelTransformer.create().filterShapes(model) { shape ->
+            when (shape) {
+                is MemberShape -> (shape !in dropMembers).also {
+                    if (!it) {
+                        logger.warning("Removed $shape from model because it is a flexible checksum member")
+                    }
+                }
+                else -> true
+            }
+        }
+    }
+}
+
+private fun findInputMember(model: Model, op: OperationShape, name: String): MemberShape =
+    model.expectShape(op.inputShape).members().first { it.memberName == name }
---
+++ codegen/smithy-aws-kotlin-codegen/src/test/kotlin/aws/sdk/kotlin/codegen/customization/RemoveChecksumSelectionFieldsTest.kt
@@ -0,0 +1,90 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0.
+ */
+package aws.sdk.kotlin.codegen.customization
+
+import software.amazon.smithy.kotlin.codegen.model.expectShape
+import software.amazon.smithy.kotlin.codegen.test.newTestContext
+import software.amazon.smithy.kotlin.codegen.test.prependNamespaceAndService
+import software.amazon.smithy.kotlin.codegen.test.toSmithyModel
+import software.amazon.smithy.model.shapes.OperationShape
+import software.amazon.smithy.model.shapes.StructureShape
+import kotlin.test.Test
+import kotlin.test.assertFalse
+import kotlin.test.assertTrue
+
+class RemoveChecksumSelectionFieldsTest {
+    @Test
+    fun testRemovingChecksumFields() {
+        val model = """
+            @httpChecksum(
+                requestValidationModeMember: "checksumMode"
+            )
+            operation GetObject {
+                input: GetObjectRequest,
+            }
+            
+            structure GetObjectRequest {
+                key: String,
+                checksumMode: ChecksumMode
+            }
+            
+            @enum([
+                {
+                    value: "ENABLED",
+                    name: "ENABLED"
+                }
+            ])
+            string ChecksumMode
+            
+            @httpChecksum(
+                requestAlgorithmMember: "checksumAlgorithm"
+            )
+            operation PutObject {
+                input: PutObjectRequest,
+            }
+            
+            structure PutObjectRequest {
+                key: String,
+                checksumAlgorithm: ChecksumAlgorithm
+            }
+            
+            @enum([
+                {
+                    value: "CRC32C",
+                    name: "CRC32C"
+                },
+                {
+                    value: "CRC32",
+                    name: "CRC32"
+                },
+                {
+                    value: "SHA1",
+                    name: "SHA1"
+                },
+                {
+                    value: "SHA256",
+                    name: "SHA256"
+                }
+            ])
+            string ChecksumAlgorithm
+        """.prependNamespaceAndService(
+            imports = listOf("aws.protocols#httpChecksum"),
+            operations = listOf("GetObject", "PutObject"),
+        ).toSmithyModel()
+
+        val ctx = model.newTestContext()
+        val transformed = RemoveChecksumSelectionFields().preprocessModel(model, ctx.generationCtx.settings)
+
+        val getOp = transformed.expectShape<OperationShape>("com.test#PutObject")
+        val getReq = transformed.expectShape<StructureShape>(getOp.inputShape)
+        assertTrue("key" in getReq.memberNames, "Expected 'key' in request object")
+        assertFalse("checksumMode" in getReq.memberNames, "Unexpected 'checksumMode' in request object")
+
+        val putOp = transformed.expectShape<OperationShape>("com.test#PutObject")
+        val putReq = transformed.expectShape<StructureShape>(putOp.inputShape)
+        assertTrue("key" in putReq.memberNames, "Expected 'key' in request object")
+        assertFalse("checksumAlgorithm" in putReq.memberNames, "Unexpected 'checksumAlgorithm' in request object")
+    }
+}
