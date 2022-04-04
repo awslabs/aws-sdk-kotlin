d3fe760219d69aeaf26403b1bf6aade7f14e329f
Aaron J Todd <todaaron@amazon.com>
2022-04-04 10:29:31 -0400

    fix init script

--- gradle/catapult-init.gradle.kts
+++ gradle/catapult-init.gradle.kts
@@ -1,5 +1,12 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0.
+ */
+
+ // Init script used by build and release automation
+
 gradle.projectsLoaded {
     rootProject.allprojects {
-        buildDir = rootProject.file("build-dir"/${rootProject.name}/${project.name}")
+        buildDir = rootProject.file("build-dir/${rootProject.name}/${project.name}")
     }
 }
