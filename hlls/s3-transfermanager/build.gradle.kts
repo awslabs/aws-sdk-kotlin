7aeb035adcca719033f6bf8d2a1583c3dc9c3bbf
Ian Botsford <83236726+ianbotsf@users.noreply.github.com>
2022-06-21 13:14:51 -0700

    WIP proposal for structure of S3 Transfer Manager project (and HLLs in general) (#640)

---
+++ hlls/s3-transfermanager/build.gradle.kts
@@ -0,0 +1,22 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0.
+ */
+
+description = "S3 Transfer Manager"
+extra["displayName"] = "AWS :: SDK :: Kotlin :: S3 Transfer Manager"
+extra["moduleName"] = "aws.sdk.kotlin.s3.transfermanager"
+
+val releasedSdkVersion: String by project
+val smithyKotlinVersion: String by project
+
+kotlin {
+    sourceSets {
+        commonMain {
+            dependencies {
+                implementation("aws.smithy.kotlin:logging:$smithyKotlinVersion")
+                implementation("aws.sdk.kotlin:s3:$releasedSdkVersion")
+            }
+        }
+    }
+}
