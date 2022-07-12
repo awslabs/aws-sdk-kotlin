43cc5e868420c9dc69b8a637670f0e7bcb568f4f
LDsBF <tw2774@columbia.edu>
2022-07-11 22:17:35 -0700

    modify upload() and Operation to multi coroutine version

--- hlls/s3-transfermanager/jvm/src/aws/sdk/kotlin/s3/transfermanager/handler/Operation.kt
+++ hlls/s3-transfermanager/jvm/src/aws/sdk/kotlin/s3/transfermanager/handler/Operation.kt
@@ -1,8 +1,9 @@
 package aws.sdk.kotlin.s3.transfermanager.handler
 
 import aws.sdk.kotlin.s3.transfermanager.data.Progress
+import kotlinx.coroutines.Deferred
 
-public interface Operation {
+public interface Operation : Deferred<Unit> {
 // inherits await(), cancel(), cancelAndJoin(), etc. from Deferred from Job
 
     // class reflecting transfer progress and mark whether it is completed
