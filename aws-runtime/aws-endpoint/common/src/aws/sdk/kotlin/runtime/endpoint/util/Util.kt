fe978d5e5e5e1785e1cf4d1255e7ae5b8e032a4e
Luc Talatinian <lucix@amazon.com>
2022-08-11 23:15:26 -0400

    feat: implement aws-specific endpoints v2 stdlib

---
+++ aws-runtime/aws-endpoint/common/src/aws/sdk/kotlin/runtime/endpoint/util/Util.kt
@@ -0,0 +1,103 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0.
+ */
+package aws.sdk.kotlin.runtime.endpoint.util
+
+import aws.sdk.kotlin.runtime.InternalSdkApi
+
+// the number of top-level components an arn contains (separated by colons)
+private const val arnComponentCount = 6
+
+@InternalSdkApi
+public fun partition(region: String): Partition =
+    when {
+        region.startsWith("cn-") -> AwsCnPartition
+        region.startsWith("us-gov-") -> AwsUsGovPartition
+        region.startsWith("us-iso-") -> AwsIsoPartition
+        region.startsWith("us-iso-b-") -> AwsIsoBPartition
+        else -> AwsPartition
+    }
+
+@InternalSdkApi
+public fun parseArn(value: String): Arn? {
+    val split = value.split(':', limit = arnComponentCount)
+    if (split[0] != "arn") return null
+    if (split.size != arnComponentCount) return null
+
+    return Arn(
+        split[1],
+        split[2],
+        split[3],
+        split[4],
+        split[5].split(':', '/'),
+    )
+}
+
+@InternalSdkApi
+public data class Partition(
+    public val name: String,
+    public val dnsSuffix: String,
+    public val dnsDualStackSuffix: String,
+    public val supportsFips: Boolean,
+    public val supportsDualStack: Boolean,
+)
+
+@InternalSdkApi
+public val AwsPartition: Partition =
+    Partition(
+        name = "aws",
+        dnsSuffix = "amazonaws.com",
+        dnsDualStackSuffix = "api.aws",
+        supportsFips = true,
+        supportsDualStack = true,
+    )
+
+@InternalSdkApi
+public val AwsCnPartition: Partition =
+    Partition(
+        name = "aws-cn",
+        dnsSuffix = "amazonaws.com.cn",
+        dnsDualStackSuffix = "cndod",
+        supportsFips = false,
+        supportsDualStack = true,
+    )
+
+@InternalSdkApi
+public val AwsIsoPartition: Partition =
+    Partition(
+        name = "aws-iso",
+        dnsSuffix = "c2s.ic.gov",
+        dnsDualStackSuffix = "cn-todo",
+        supportsFips = true,
+        supportsDualStack = false,
+    )
+
+@InternalSdkApi
+public val AwsIsoBPartition: Partition =
+    Partition(
+        name = "aws-iso-b",
+        dnsSuffix = "sc2s.sgov.gov",
+        dnsDualStackSuffix = "cn-todo",
+        supportsFips = true,
+        supportsDualStack = false,
+    )
+
+@InternalSdkApi
+public val AwsUsGovPartition: Partition =
+    Partition(
+        name = "aws-us-gov",
+        dnsSuffix = "amazonaws.com",
+        dnsDualStackSuffix = "cn-todo",
+        supportsFips = true,
+        supportsDualStack = true,
+    )
+
+@InternalSdkApi
+public data class Arn(
+    public val partition: String,
+    public val service: String,
+    public val region: String,
+    public val accountId: String,
+    public val resourceId: List<String>,
+)
