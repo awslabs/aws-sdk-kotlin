e9c80f54c1bde63fae68bf4c72fab77fece73a3b
Luc Talatinian <lucix@amazon.com>
2022-08-29 18:26:42 -0400

    correct a comment format

--- aws-runtime/aws-endpoint/common/src/aws/sdk/kotlin/runtime/endpoint/functions/Functions.kt
+++ aws-runtime/aws-endpoint/common/src/aws/sdk/kotlin/runtime/endpoint/functions/Functions.kt
@@ -2,9 +2,7 @@
  * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
  * SPDX-License-Identifier: Apache-2.0.
  */
-/**
- * This package implements AWS-specific standard library functions used by endpoint resolvers of AWS SDKs.
- */
+// This package implements AWS-specific standard library functions used by endpoint resolvers of AWS SDKs.
 package aws.sdk.kotlin.runtime.endpoint.functions
 
 import aws.sdk.kotlin.runtime.InternalSdkApi
