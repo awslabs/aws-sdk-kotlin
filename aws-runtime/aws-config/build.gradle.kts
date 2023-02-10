79c68f7ff59773589772741e3e2b3ae4babbc8c8
Matas Lauzadis <lauzmata@amazon.com>
2023-02-10 14:50:45 -0600

    Remove atomicfu plugin

--- aws-runtime/aws-config/build.gradle.kts
+++ aws-runtime/aws-config/build.gradle.kts
@@ -18,17 +18,10 @@ val coroutinesVersion: String by project
 val atomicFuVersion: String by project
 
 buildscript {
-    val atomicFuVersion: String by project
-
     repositories {
         mavenCentral()
     }
-
-    dependencies {
-        classpath("org.jetbrains.kotlinx:atomicfu-gradle-plugin:$atomicFuVersion")
-    }
 }
-apply(plugin = "kotlinx-atomicfu")
 
 kotlin {
     sourceSets {
