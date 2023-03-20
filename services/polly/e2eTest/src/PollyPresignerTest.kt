13833d9d556cc7103e5624b09e3ef82fcedab009
Matas <lauzmata@amazon.com>
2023-03-20 09:11:33 -0500

    misc: add test for `ServicePresignConfig`-based presigning (#870)

--- services/polly/e2eTest/src/PollyPresignerTest.kt
+++ services/polly/e2eTest/src/PollyPresignerTest.kt
@@ -4,11 +4,16 @@
  */
 package aws.sdk.kotlin.services.polly
 
+import aws.sdk.kotlin.runtime.auth.credentials.DefaultChainCredentialsProvider
+import aws.sdk.kotlin.services.polly.endpoints.DefaultEndpointProvider
+import aws.sdk.kotlin.services.polly.endpoints.EndpointParameters
 import aws.sdk.kotlin.services.polly.model.OutputFormat
 import aws.sdk.kotlin.services.polly.model.SynthesizeSpeechRequest
 import aws.sdk.kotlin.services.polly.model.VoiceId
+import aws.sdk.kotlin.services.polly.presigners.PollyPresignConfig
 import aws.sdk.kotlin.services.polly.presigners.presign
 import aws.sdk.kotlin.testing.withAllEngines
+import aws.smithy.kotlin.runtime.auth.awssigning.SigningContextualizedEndpoint
 import aws.smithy.kotlin.runtime.http.SdkHttpClient
 import aws.smithy.kotlin.runtime.http.response.complete
 import kotlinx.coroutines.runBlocking
@@ -33,6 +38,39 @@ class PollyPresignerTest {
 
         val client = PollyClient { region = "us-east-1" }
         val presignedRequest = request.presign(client.config, 10.seconds)
+
+        withAllEngines { engine ->
+            val httpClient = SdkHttpClient(engine)
+
+            val call = httpClient.call(presignedRequest)
+            call.complete()
+
+            assertEquals(200, call.response.status.value, "presigned polly request failed for engine: $engine")
+        }
+    }
+
+    @Test
+    fun presignConfigBasedPresign() = runBlocking {
+        val request = SynthesizeSpeechRequest {
+            voiceId = VoiceId.Salli
+            outputFormat = OutputFormat.Pcm
+            text = "hello world"
+        }
+
+        val presignConfig = PollyPresignConfig {
+            credentialsProvider = DefaultChainCredentialsProvider()
+            endpointProvider = {
+                val endpoint = DefaultEndpointProvider().resolveEndpoint(
+                    EndpointParameters.invoke {
+                        region = it.region
+                    },
+                )
+                SigningContextualizedEndpoint(endpoint, it)
+            }
+            region = "us-east-1"
+        }
+
+        val presignedRequest = request.presign(presignConfig, 10.seconds)
 
         withAllEngines { engine ->
             val httpClient = SdkHttpClient(engine)
