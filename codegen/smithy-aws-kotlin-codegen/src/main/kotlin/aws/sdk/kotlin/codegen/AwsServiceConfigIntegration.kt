e1e8c2ea995ca7a9d31e2e0f1a9d8278a034eed2
Luc Talatinian <102624213+lucix-aws@users.noreply.github.com>
2023-03-23 14:02:59 -0400

    misc: add advanced endpoint config guide (#878)

--- codegen/smithy-aws-kotlin-codegen/src/main/kotlin/aws/sdk/kotlin/codegen/AwsServiceConfigIntegration.kt
+++ codegen/smithy-aws-kotlin-codegen/src/main/kotlin/aws/sdk/kotlin/codegen/AwsServiceConfigIntegration.kt
@@ -8,6 +8,7 @@ import software.amazon.smithy.kotlin.codegen.core.*
 import software.amazon.smithy.kotlin.codegen.integration.KotlinIntegration
 import software.amazon.smithy.kotlin.codegen.integration.SectionWriterBinding
 import software.amazon.smithy.kotlin.codegen.lang.KotlinTypes
+import software.amazon.smithy.kotlin.codegen.model.asNullable
 import software.amazon.smithy.kotlin.codegen.model.boxed
 import software.amazon.smithy.kotlin.codegen.rendering.*
 import software.amazon.smithy.kotlin.codegen.rendering.util.ConfigProperty
@@ -76,9 +77,15 @@ class AwsServiceConfigIntegration : KotlinIntegration {
 
         val EndpointUrlProp = ConfigProperty {
             name = "endpointUrl"
-            symbol = RuntimeTypes.Core.Net.Url.toBuilder().boxed().build()
+            symbol = RuntimeTypes.Core.Net.Url.asNullable()
             documentation = """
-                A custom endpoint to use when making requests.
+                A custom endpoint to route requests to. The endpoint set here is passed to the configured
+                [endpointProvider], which may inspect and modify it as needed.
+
+                Setting a custom endpointUrl should generally be preferred to overriding the [endpointProvider] and is
+                the recommended way to route requests to development or preview instances of a service.
+
+                **This is an advanced config option.**
             """.trimIndent()
             propertyType = ConfigPropertyType.SymbolDefault
         }
