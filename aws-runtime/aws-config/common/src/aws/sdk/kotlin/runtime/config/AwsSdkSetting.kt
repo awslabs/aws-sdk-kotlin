1164f9c22cc82f909e24bdb10759034463a1bd14
Ian Smith Botsford <issmith@amazon.com>
2023-04-24 17:17:38 +0000

    lint; swap envVar and sysProp to match evaluation order

--- aws-runtime/aws-config/common/src/aws/sdk/kotlin/runtime/config/AwsSdkSetting.kt
+++ aws-runtime/aws-config/common/src/aws/sdk/kotlin/runtime/config/AwsSdkSetting.kt
@@ -5,11 +5,9 @@
 
 package aws.sdk.kotlin.runtime.config
 
-import aws.sdk.kotlin.runtime.ConfigurationException
 import aws.sdk.kotlin.runtime.InternalSdkApi
 import aws.smithy.kotlin.runtime.client.config.RetryMode
 import aws.smithy.kotlin.runtime.config.*
-import aws.smithy.kotlin.runtime.util.PlatformEnvironProvider
 
 // NOTE: The JVM property names MUST match the ones defined in the Java SDK for any setting added.
 // see: https://github.com/aws/aws-sdk-java-v2/blob/master/core/sdk-core/src/main/java/software/amazon/awssdk/core/SdkSystemSetting.java
@@ -25,7 +23,7 @@ public object AwsSdkSetting {
      *
      * This value will not be ignored if the [AwsSecretAccessKey] is not specified.
      */
-    public val AwsAccessKeyId: EnvironmentSetting<String> = strEnvSetting("AWS_ACCESS_KEY_ID", "aws.accessKeyId")
+    public val AwsAccessKeyId: EnvironmentSetting<String> = strEnvSetting("aws.accessKeyId", "AWS_ACCESS_KEY_ID")
 
     /**
      * Configure the AWS secret access key.
@@ -33,122 +31,125 @@ public object AwsSdkSetting {
      * This value will not be ignored if the [AwsAccessKeyId] is not specified.
      */
     public val AwsSecretAccessKey: EnvironmentSetting<String> =
-        strEnvSetting("AWS_SECRET_ACCESS_KEY", "aws.secretAccessKey")
+        strEnvSetting("aws.secretAccessKey", "AWS_SECRET_ACCESS_KEY")
 
     /**
      * Configure the AWS session token.
      */
-    public val AwsSessionToken: EnvironmentSetting<String> = strEnvSetting("AWS_SESSION_TOKEN", "aws.sessionToken")
+    public val AwsSessionToken: EnvironmentSetting<String> = strEnvSetting("aws.sessionToken", "AWS_SESSION_TOKEN")
 
     /**
      * Configure the default region.
      */
-    public val AwsRegion: EnvironmentSetting<String> = strEnvSetting("AWS_REGION", "aws.region")
+    public val AwsRegion: EnvironmentSetting<String> = strEnvSetting("aws.region", "AWS_REGION")
 
     /**
      * Configure the default path to the shared config file.
      */
-    public val AwsConfigFile: EnvironmentSetting<String> = strEnvSetting("AWS_CONFIG_FILE", "aws.configFile")
+    public val AwsConfigFile: EnvironmentSetting<String> = strEnvSetting("aws.configFile", "AWS_CONFIG_FILE")
 
     /**
      * Configure the default path to the shared credentials profile file.
      */
     public val AwsSharedCredentialsFile: EnvironmentSetting<String> =
-        strEnvSetting("AWS_SHARED_CREDENTIALS_FILE", "aws.sharedCredentialsFile")
+        strEnvSetting("aws.sharedCredentialsFile", "AWS_SHARED_CREDENTIALS_FILE")
 
     /**
-     * The execution environment of the SDK user. This is automatically set in certain environments by the underlying AWS service.
-     * For example, AWS Lambda will automatically specify a runtime indicating that the SDK is being used within Lambda.
+     * The execution environment of the SDK user. This is automatically set in certain environments by the underlying
+     * AWS service. For example, AWS Lambda will automatically specify a runtime indicating that the SDK is being used
+     * within Lambda.
      */
     public val AwsExecutionEnv: EnvironmentSetting<String> =
-        strEnvSetting("AWS_EXECUTION_ENV", "aws.executionEnvironment")
+        strEnvSetting("aws.executionEnvironment", "AWS_EXECUTION_ENV")
 
     /**
      *  The name of the default profile that should be loaded from config
      */
-    public val AwsProfile: EnvironmentSetting<String> = strEnvSetting("AWS_PROFILE", "aws.profile").orElse("default")
+    public val AwsProfile: EnvironmentSetting<String> = strEnvSetting("aws.profile", "AWS_PROFILE").orElse("default")
 
     /**
      * Whether to load information such as credentials, regions from EC2 Metadata instance service.
      */
     public val AwsEc2MetadataDisabled: EnvironmentSetting<Boolean> =
-        boolEnvSetting("AWS_EC2_METADATA_DISABLED", "aws.disableEc2Metadata").orElse(false)
+        boolEnvSetting("aws.disableEc2Metadata", "AWS_EC2_METADATA_DISABLED").orElse(false)
 
     /**
      * The EC2 instance metadata service endpoint.
      *
-     * This allows a service running in EC2 to automatically load its credentials and region without needing to configure them
-     * directly.
+     * This allows a service running in EC2 to automatically load its credentials and region without needing to
+     * configure them directly.
      */
     public val AwsEc2MetadataServiceEndpoint: EnvironmentSetting<String> =
-        strEnvSetting("AWS_EC2_METADATA_SERVICE_ENDPOINT", "aws.ec2MetadataServiceEndpoint")
+        strEnvSetting("aws.ec2MetadataServiceEndpoint", "AWS_EC2_METADATA_SERVICE_ENDPOINT")
 
     /**
      * The endpoint mode to use when connecting to the EC2 metadata service endpoint
      */
     public val AwsEc2MetadataServiceEndpointMode: EnvironmentSetting<String> =
-        strEnvSetting("AWS_EC2_METADATA_SERVICE_ENDPOINT_MODE", "aws.ec2MetadataServiceEndpointMode")
+        strEnvSetting("aws.ec2MetadataServiceEndpointMode", "AWS_EC2_METADATA_SERVICE_ENDPOINT_MODE")
 
-    // TODO - Currently env/system properties around role ARN, role session name, etc are restricted to the STS web identity provider.
-    //        They should be applied more broadly but this needs fleshed out across AWS SDKs before we can do so.
+    // TODO Currently env/system properties around role ARN, role session name, etc are restricted to the STS web
+    //  identity provider. They should be applied more broadly but this needs fleshed out across AWS SDKs before we can
+    //  do so.
 
     /**
      * The ARN of a role to assume
      */
-    public val AwsRoleArn: EnvironmentSetting<String> = strEnvSetting("AWS_ROLE_ARN", "aws.roleArn")
+    public val AwsRoleArn: EnvironmentSetting<String> = strEnvSetting("aws.roleArn", "AWS_ROLE_ARN")
 
     /**
      * The session name to use for assumed roles
      */
     public val AwsRoleSessionName: EnvironmentSetting<String> =
-        strEnvSetting("AWS_ROLE_SESSION_NAME", "aws.roleSessionName")
+        strEnvSetting("aws.roleSessionName", "AWS_ROLE_SESSION_NAME")
 
     /**
      * The AWS web identity token file path
      */
     public val AwsWebIdentityTokenFile: EnvironmentSetting<String> =
-        strEnvSetting("AWS_WEB_IDENTITY_TOKEN_FILE", "aws.webIdentityTokenFile")
+        strEnvSetting("aws.webIdentityTokenFile", "AWS_WEB_IDENTITY_TOKEN_FILE")
 
     /**
-     * The elastic container metadata service path that should be called by the [aws.sdk.kotlin.runtime.auth.credentials.EcsCredentialsProvider]
-     * when loading credentials from the container metadata service.
+     * The elastic container metadata service path that should be called by the
+     * [aws.sdk.kotlin.runtime.auth.credentials.EcsCredentialsProvider] when loading credentials from the container
+     * metadata service.
      */
     public val AwsContainerCredentialsRelativeUri: EnvironmentSetting<String> =
-        strEnvSetting("AWS_CONTAINER_CREDENTIALS_RELATIVE_URI", "aws.containerCredentialsPath")
+        strEnvSetting("aws.containerCredentialsPath", "AWS_CONTAINER_CREDENTIALS_RELATIVE_URI")
 
     /**
      * The full URI path to a localhost metadata service to be used. This is ignored if
      * [AwsContainerCredentialsRelativeUri] is set.
      */
     public val AwsContainerCredentialsFullUri: EnvironmentSetting<String> =
-        strEnvSetting("AWS_CONTAINER_CREDENTIALS_FULL_URI", "aws.containerCredentialsFullUri")
+        strEnvSetting("aws.containerCredentialsFullUri", "AWS_CONTAINER_CREDENTIALS_FULL_URI")
 
     /**
      * An authorization token to pass to a container metadata service.
      */
     public val AwsContainerAuthorizationToken: EnvironmentSetting<String> =
-        strEnvSetting("AWS_CONTAINER_AUTHORIZATION_TOKEN", "aws.containerAuthorizationToken")
+        strEnvSetting("aws.containerAuthorizationToken", "AWS_CONTAINER_AUTHORIZATION_TOKEN")
 
     /**
      * The maximum number of request attempts to perform. This is one more than the number of retries, so
      * aws.maxAttempts = 1 will have 0 retries.
      */
-    public val AwsMaxAttempts: EnvironmentSetting<Int> = intEnvSetting("AWS_MAX_ATTEMPTS", "aws.maxAttempts")
+    public val AwsMaxAttempts: EnvironmentSetting<Int> = intEnvSetting("aws.maxAttempts", "AWS_MAX_ATTEMPTS")
 
     /**
      * Which RetryMode to use for the default RetryPolicy, when one is not specified at the client level.
      */
-    public val AwsRetryMode: EnvironmentSetting<RetryMode> = enumEnvSetting("AWS_RETRY_MODE", "aws.retryMode")
+    public val AwsRetryMode: EnvironmentSetting<RetryMode> = enumEnvSetting("aws.retryMode", "AWS_RETRY_MODE")
 
     /**
      * Whether to use FIPS endpoints when making requests.
      */
     public val AwsUseFipsEndpoint: EnvironmentSetting<Boolean> =
-        boolEnvSetting("AWS_USE_FIPS_ENDPOINT", "aws.useFipsEndpoint")
+        boolEnvSetting("aws.useFipsEndpoint", "AWS_USE_FIPS_ENDPOINT")
 
     /**
      * Whether to use dual-stack endpoints when making requests.
      */
     public val AwsUseDualStackEndpoint: EnvironmentSetting<Boolean> =
-        boolEnvSetting("AWS_USE_DUALSTACK_ENDPOINT", "aws.useDualstackEndpoint")
+        boolEnvSetting("aws.useDualstackEndpoint", "AWS_USE_DUALSTACK_ENDPOINT")
 }
