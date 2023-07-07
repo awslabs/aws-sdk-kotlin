3866fd286626641856dc3bc0707f11c1c25fc6d9
Aaron Todd <aajtodd@users.noreply.github.com>
2023-07-07 14:38:53 -0400

    optin to experimental telemetry api (#973)

--- aws-runtime/aws-config/common/src/aws/sdk/kotlin/runtime/config/AbstractAwsSdkClientFactory.kt
+++ aws-runtime/aws-config/common/src/aws/sdk/kotlin/runtime/config/AbstractAwsSdkClientFactory.kt
@@ -12,6 +12,7 @@ import aws.sdk.kotlin.runtime.config.profile.AwsProfile
 import aws.sdk.kotlin.runtime.config.profile.loadAwsSharedConfig
 import aws.sdk.kotlin.runtime.config.retries.resolveRetryStrategy
 import aws.sdk.kotlin.runtime.region.resolveRegion
+import aws.smithy.kotlin.runtime.ExperimentalApi
 import aws.smithy.kotlin.runtime.client.RetryStrategyClientConfig
 import aws.smithy.kotlin.runtime.client.SdkClient
 import aws.smithy.kotlin.runtime.client.SdkClientConfig
@@ -46,6 +47,7 @@ public abstract class AbstractAwsSdkClientFactory<
     /**
      * Construct a [TClient] by resolving the configuration from the current environment.
      */
+    @OptIn(ExperimentalApi::class)
     public suspend fun fromEnvironment(block: (TConfigBuilder.() -> Unit)? = null): TClient {
         val builder = builder()
         val config = builder.config
