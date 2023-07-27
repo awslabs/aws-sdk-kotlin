12d8d6115f664613101b12f86329157db5d591b9
0marperez <operez899999@gmail.com>
2023-07-27 13:18:18 -0400

    Automatically detect s3unwrappedXmlOutput trait

--- codegen/smithy-aws-kotlin-codegen/src/main/kotlin/aws/sdk/kotlin/codegen/customization/s3/UnwrappedXmlOutputIntegration.kt
+++ codegen/smithy-aws-kotlin-codegen/src/main/kotlin/aws/sdk/kotlin/codegen/customization/s3/UnwrappedXmlOutputIntegration.kt
@@ -4,11 +4,14 @@
  */
 package aws.sdk.kotlin.codegen.customization.s3
 
+import software.amazon.smithy.aws.traits.customizations.S3UnwrappedXmlOutputTrait
 import software.amazon.smithy.kotlin.codegen.KotlinSettings
 import software.amazon.smithy.kotlin.codegen.integration.KotlinIntegration
 import software.amazon.smithy.kotlin.codegen.model.expectShape
+import software.amazon.smithy.kotlin.codegen.model.hasTrait
 import software.amazon.smithy.kotlin.codegen.model.traits.UnwrappedXmlOutput
 import software.amazon.smithy.model.Model
+import software.amazon.smithy.model.shapes.OperationShape
 import software.amazon.smithy.model.shapes.ServiceShape
 import software.amazon.smithy.model.shapes.StructureShape
 import software.amazon.smithy.model.transform.ModelTransformer
@@ -18,22 +21,37 @@ private val SHAPE_IDS = setOf(
 )
 
 /**
- * Applies the [UnwrappedXmlOutput] custom-made [annotation trait](https://smithy.io/2.0/spec/model.html?highlight=annotation#annotation-traits) to a manually-curated list of structures
+ * Applies the [UnwrappedXmlOutput] custom-made [annotation trait](https://smithy.io/2.0/spec/model.html?highlight=annotation#annotation-traits) to structures
  * to mark when special unwrapped xml output deserialization is required.
  */
 class UnwrappedXmlOutputIntegration : KotlinIntegration {
     override fun enabledForService(model: Model, settings: KotlinSettings): Boolean =
         model.expectShape<ServiceShape>(settings.service).isS3
 
-    override fun preprocessModel(model: Model, settings: KotlinSettings): Model = ModelTransformer
-        .create()
-        .mapShapes(model) { shape ->
-            when {
-                shape.id.toString() in SHAPE_IDS -> {
-                    check(shape is StructureShape) { "Cannot apply UnwrappedXMLOutput to non-structure shape" }
-                    shape.toBuilder().addTrait(UnwrappedXmlOutput()).build()
+    override fun preprocessModel(model: Model, settings: KotlinSettings): Model {
+        val unwrappedStructures = mutableSetOf<String>()
+        ModelTransformer
+            .create()
+            .mapShapes(model) { shape ->
+                when {
+                    shape is OperationShape && shape.hasTrait<S3UnwrappedXmlOutputTrait>() -> {
+                        if (shape.outputShape.toString() != "smithy.api#Unit") unwrappedStructures.add(shape.outputShape.toString())
+                        shape
+                    }
+                    else -> shape
                 }
-                else -> shape
-            }
         }
+
+        return ModelTransformer
+            .create()
+            .mapShapes(model) { shape ->
+                when {
+                    shape.id.toString() in unwrappedStructures -> {
+                        check(shape is StructureShape) { "Cannot apply UnwrappedXMLOutput to non-structure shape" }
+                        shape.toBuilder().addTrait(UnwrappedXmlOutput()).build()
+                    }
+                    else -> shape
+                }
+            }
+    }
 }
