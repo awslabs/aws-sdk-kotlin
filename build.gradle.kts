206dcd63a760a2fe42a46999b7ad196adabdec96
Aaron J Todd <todaaron@amazon.com>
2023-08-16 08:29:21 -0400

    use tagged version

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,8 +14,7 @@ buildscript {
         // only need to include it here, imports in subprojects will work automagically
         classpath("aws.sdk.kotlin:build-plugins") {
             version {
-                // require("0.2.0")
-                branch = "publish-plugin"
+                require("0.2.1")
             }
         }
     }
