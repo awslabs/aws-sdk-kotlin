aba44eb5ce56881bef2c224ae6aa459b441bcc02
Matas Lauzadis <lauzmata@amazon.com>
2023-10-06 09:20:50 -0400

    ClockSkew integration / interceptor

---
+++ codegen/smithy-aws-kotlin-codegen/src/main/kotlin/aws/sdk/kotlin/codegen/customization/ClockSkew.kt
@@ -0,0 +1,31 @@
+package aws.sdk.kotlin.codegen.customization
+
+import software.amazon.smithy.kotlin.codegen.KotlinSettings
+import software.amazon.smithy.kotlin.codegen.core.KotlinWriter
+import software.amazon.smithy.kotlin.codegen.core.RuntimeTypes
+import software.amazon.smithy.kotlin.codegen.integration.KotlinIntegration
+import software.amazon.smithy.kotlin.codegen.rendering.protocol.ProtocolGenerator
+import software.amazon.smithy.kotlin.codegen.rendering.protocol.ProtocolMiddleware
+import software.amazon.smithy.model.Model
+import software.amazon.smithy.model.shapes.OperationShape
+
+/**
+ * Adds a middleware that enables sending flexible checksums during an HTTP request
+ */
+class ClockSkew : KotlinIntegration {
+    override fun enabledForService(model: Model, settings: KotlinSettings) = true
+
+    override fun customizeMiddleware(ctx: ProtocolGenerator.GenerationContext, resolved: List<ProtocolMiddleware>) =
+        resolved + clockSkewMiddleware
+
+    private val clockSkewMiddleware = object : ProtocolMiddleware {
+        override val name: String = "ClockSkew"
+
+        override fun isEnabledFor(ctx: ProtocolGenerator.GenerationContext, op: OperationShape): Boolean = true
+
+        override fun render(ctx: ProtocolGenerator.GenerationContext, op: OperationShape, writer: KotlinWriter) {
+            val interceptorSymbol = RuntimeTypes.HttpClient.Interceptors.ClockSkewInterceptor
+            writer.write("op.interceptors.add(#T())", interceptorSymbol)
+        }
+    }
+}
