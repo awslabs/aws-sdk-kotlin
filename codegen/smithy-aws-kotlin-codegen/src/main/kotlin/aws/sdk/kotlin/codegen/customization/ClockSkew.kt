47a28a255b4bd9908a4e600514cee79ac67f106e
Matas Lauzadis <lauzmata@amazon.com>
2023-10-06 12:26:53 -0400

    Remove unnecessary overrides

--- codegen/smithy-aws-kotlin-codegen/src/main/kotlin/aws/sdk/kotlin/codegen/customization/ClockSkew.kt
+++ codegen/smithy-aws-kotlin-codegen/src/main/kotlin/aws/sdk/kotlin/codegen/customization/ClockSkew.kt
@@ -4,28 +4,22 @@
  */
 package aws.sdk.kotlin.codegen.customization
 
-import software.amazon.smithy.kotlin.codegen.KotlinSettings
 import software.amazon.smithy.kotlin.codegen.core.KotlinWriter
 import software.amazon.smithy.kotlin.codegen.core.RuntimeTypes
 import software.amazon.smithy.kotlin.codegen.integration.KotlinIntegration
 import software.amazon.smithy.kotlin.codegen.rendering.protocol.ProtocolGenerator
 import software.amazon.smithy.kotlin.codegen.rendering.protocol.ProtocolMiddleware
-import software.amazon.smithy.model.Model
 import software.amazon.smithy.model.shapes.OperationShape
 
 /**
- * Adds a middleware that enables sending flexible checksums during an HTTP request
+ * Adds a middleware which applies an interceptor that detects and corrects clock skew
  */
 class ClockSkew : KotlinIntegration {
-    override fun enabledForService(model: Model, settings: KotlinSettings) = true
-
     override fun customizeMiddleware(ctx: ProtocolGenerator.GenerationContext, resolved: List<ProtocolMiddleware>) =
         resolved + clockSkewMiddleware
 
     private val clockSkewMiddleware = object : ProtocolMiddleware {
         override val name: String = "ClockSkew"
-
-        override fun isEnabledFor(ctx: ProtocolGenerator.GenerationContext, op: OperationShape): Boolean = true
 
         override fun renderProperties(writer: KotlinWriter) {
             val interceptorSymbol = RuntimeTypes.HttpClient.Interceptors.ClockSkewInterceptor
