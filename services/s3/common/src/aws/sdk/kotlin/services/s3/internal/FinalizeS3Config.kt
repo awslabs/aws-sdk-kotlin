6eaa95bd98bae2542fc6bb752a5847b033d68962
0marperez <operez899999@gmail.com>
2023-10-18 16:05:41 -0400

    Removed CLI only S3 config options & refactor others to be top level

--- services/s3/common/src/aws/sdk/kotlin/services/s3/internal/FinalizeS3Config.kt
+++ services/s3/common/src/aws/sdk/kotlin/services/s3/internal/FinalizeS3Config.kt
@@ -4,7 +4,6 @@
  */
 package aws.sdk.kotlin.services.s3.internal
 
-import aws.sdk.kotlin.runtime.ConfigurationException
 import aws.sdk.kotlin.runtime.config.profile.AwsProfile
 import aws.sdk.kotlin.runtime.config.profile.AwsSharedConfig
 import aws.sdk.kotlin.runtime.config.profile.getBooleanOrNull
@@ -13,19 +12,13 @@ import aws.smithy.kotlin.runtime.util.LazyAsyncValue
 
 internal suspend fun finalizeS3Config(builder: S3Client.Builder, sharedConfig: LazyAsyncValue<AwsSharedConfig>) {
     sharedConfig.get().activeProfile.let {
-        builder.config.forcePathStyle = builder.config.forcePathStyle ?: it.forcePathStyle
-        builder.config.enableAccelerate = builder.config.enableAccelerate ?: it.enableAccelerate
+        builder.config.useArnRegion = builder.config.useArnRegion ?: it.useArnRegion
+        builder.config.disableMrap = builder.config.disableMrap ?: it.disableMrap
     }
 }
 
-private val AwsProfile.forcePathStyle: Boolean?
-    get() = getOrNull("s3", "addressing_style")?.lowercase()?.let {
-        when (it) {
-            "virtual", "auto" -> false
-            "path" -> true
-            else -> throw ConfigurationException("invalid value '$it' for config property s3.addressing_style")
-        }
-    }
+private val AwsProfile.useArnRegion: Boolean?
+    get() = getBooleanOrNull("s3_use_arn_region")
 
-private val AwsProfile.enableAccelerate: Boolean?
-    get() = getBooleanOrNull("s3", "use_accelerate_endpoint")
+private val AwsProfile.disableMrap: Boolean?
+    get() = getBooleanOrNull("s3_disable_multiregion_access_points")
