78705487e3764ef8616c7be01b736dae39769aec
Matas Lauzadis <lauzmata@amazon.com>
2024-02-07 12:47:53 -0500

    create S3ExpressCredentialsProvider interface

--- services/s3/common/src/aws/sdk/kotlin/services/s3/internal/S3ExpressCredentialsProvider.kt
+++ services/s3/common/src/aws/sdk/kotlin/services/s3/internal/S3ExpressCredentialsProvider.kt
@@ -5,22 +5,23 @@
 package aws.sdk.kotlin.services.s3.internal
 
 import aws.sdk.kotlin.runtime.auth.S3ExpressAttributes
-import aws.sdk.kotlin.runtime.auth.credentials.internal.credentials
-import aws.sdk.kotlin.services.s3.*
-import aws.sdk.kotlin.services.s3.model.CreateSessionRequest
 import aws.smithy.kotlin.runtime.auth.awscredentials.Credentials
 import aws.smithy.kotlin.runtime.auth.awscredentials.CredentialsProvider
 import aws.smithy.kotlin.runtime.auth.awscredentials.CloseableCredentialsProvider
 import aws.smithy.kotlin.runtime.collections.Attributes
 import aws.smithy.kotlin.runtime.collections.get
 import aws.smithy.kotlin.runtime.telemetry.logging.logger
-import aws.smithy.kotlin.runtime.util.ExpiringValue
 import kotlin.coroutines.coroutineContext
 import aws.smithy.kotlin.runtime.io.SdkManagedBase
 
-public class S3ExpressCredentialsProvider(
-    public val bootstrapCredentialsProvider: CredentialsProvider,
-) : CloseableCredentialsProvider, SdkManagedBase() {
+public interface S3ExpressCredentialsProvider: CloseableCredentialsProvider {
+    override suspend fun resolve(attributes: Attributes): Credentials
+    override fun close()
+}
+
+public class SdkS3ExpressCredentialsProvider(
+    public val bootstrapCredentialsProvider: CredentialsProvider
+) : S3ExpressCredentialsProvider, SdkManagedBase() {
     private val credentialsCache = S3ExpressCredentialsCache()
 
     override suspend fun resolve(attributes: Attributes): Credentials {
