993690db1445b42b7f339496f3ff787e8cf96b8e
Matas Lauzadis <lauzmata@amazon.com>
2024-02-15 12:11:01 -0500

    Simplify refresh logic

--- services/s3/common/src/aws/sdk/kotlin/services/s3/internal/S3ExpressCredentialsCache.kt
+++ services/s3/common/src/aws/sdk/kotlin/services/s3/internal/S3ExpressCredentialsCache.kt
@@ -9,6 +9,7 @@ import aws.smithy.kotlin.runtime.auth.awscredentials.Credentials
 import aws.smithy.kotlin.runtime.client.SdkClient
 import aws.smithy.kotlin.runtime.collections.LruCache
 import aws.smithy.kotlin.runtime.io.Closeable
+import aws.smithy.kotlin.runtime.telemetry.TelemetryProviderContext
 import aws.smithy.kotlin.runtime.telemetry.logging.logger
 import aws.smithy.kotlin.runtime.time.Clock
 import aws.smithy.kotlin.runtime.time.Instant
@@ -31,7 +32,7 @@ internal class S3ExpressCredentialsCache(
 ) : CoroutineScope, Closeable {
     override val coroutineContext: CoroutineContext = Job() + CoroutineName("S3ExpressCredentialsCacheRefresh")
 
-    private val lru = LruCache<S3ExpressCredentialsCacheKey, ExpiringValue<Credentials>>(DEFAULT_S3_EXPRESS_CACHE_SIZE)
+    private val lru = LruCache<S3ExpressCredentialsCacheKey, S3ExpressCredentialsCacheValue>(DEFAULT_S3_EXPRESS_CACHE_SIZE)
 
     init {
         launch(coroutineContext) {
@@ -39,11 +40,17 @@ internal class S3ExpressCredentialsCache(
         }
     }
 
-    suspend fun get(key: S3ExpressCredentialsCacheKey): Credentials = lru.get(key)?.takeIf { !it.isExpired(clock) }?.value
+    suspend fun get(key: S3ExpressCredentialsCacheKey): Credentials = lru
+        .get(key)
+        ?.takeIf { !it.expiringCredentials.isExpired(clock) }
+        ?.let {
+            it.usedSinceLastRefresh = true
+            it.expiringCredentials.value
+        }
         ?: (createSessionCredentials(key.bucket).also { put(key, it) }).value
 
     suspend fun put(key: S3ExpressCredentialsCacheKey, value: ExpiringValue<Credentials>) {
-        lru.put(key, value)
+        lru.put(key, S3ExpressCredentialsCacheValue(value, usedSinceLastRefresh = true))
     }
 
     private fun ExpiringValue<Credentials>.isExpired(clock: Clock): Boolean = clock.now().until(expiresAt).absoluteValue - 5.minutes <= REFRESH_BUFFER
@@ -54,30 +61,42 @@ internal class S3ExpressCredentialsCache(
      *    * the `nextRefresh` time has been reached, which is either `DEFAULT_REFRESH_PERIOD` or
      *      the soonest credentials expiration time (minus a buffer), whichever comes first.
      */
-    @OptIn(ExperimentalCoroutinesApi::class)
     private suspend fun refresh() {
+        val logger = coroutineContext.logger<S3ExpressCredentialsCache>()
         while (isActive) {
             if (lru.size == 0) {
-                println("CACHE: LRU is empty, continuing...")
+                logger.debug { "Cache is empty, waiting..." }
                 delay(5.seconds)
                 continue
             }
 
+            // Evict any credentials that weren't used since the last refresh
+            lru.entries.filter { !it.value.usedSinceLastRefresh }.forEach {
+                logger.debug { "Credentials for ${it.key.bucket} were not used since last refresh, evicting..."}
+                lru.remove(it.key)
+            }
+
+            // Mark all credentials as not used since last refresh
+            lru.entries.forEach {
+                it.value.usedSinceLastRefresh = false
+            }
+
             // Refresh any credentials which are already expired
-            val expiredEntries = lru.entries.filter { it.value.isExpired(clock) }
-            println("CACHE: ${expiredEntries.size} entries are expired, refreshing...")
+            val expiredEntries = lru.entries.filter { it.value.expiringCredentials.isExpired(clock) }
             expiredEntries.forEach { entry ->
-                lru.put(entry.key, createSessionCredentials(entry.key.bucket))
+                logger.debug { "Credentials for ${entry.key.bucket} are expired, refreshing..."}
+                lru.put(entry.key, S3ExpressCredentialsCacheValue(createSessionCredentials(entry.key.bucket), false))
             }
 
             // Find the next expiring credentials, sleep until then
-            val nextExpiringEntry = lru.entries.minBy { it.value.expiresAt }
-            println("CACHE: Current time ${clock.now()}, next expiration: ${nextExpiringEntry.value.expiresAt} ")
-            val nextRefresh = minOf(clock.now() + DEFAULT_REFRESH_PERIOD, nextExpiringEntry.value.expiresAt - REFRESH_BUFFER)
+            val nextExpiringEntry = lru.entries.minByOrNull { it.value.expiringCredentials.expiresAt }
 
-            select {
-                onTimeout(clock.now().until(nextRefresh)) {}
-            }
+            val nextRefresh = nextExpiringEntry?.let {
+                minOf(clock.now() + DEFAULT_REFRESH_PERIOD, it.value.expiringCredentials.expiresAt - REFRESH_BUFFER)
+            } ?: (clock.now() + DEFAULT_REFRESH_PERIOD)
+
+            logger.debug { "Completed credentials refresh, next attempt in ${clock.now().until(nextRefresh)}" }
+            delay(clock.now().until(nextRefresh))
         }
     }
 
@@ -104,4 +123,9 @@ internal class S3ExpressCredentialsCache(
 internal data class S3ExpressCredentialsCacheKey(
     val bucket: String,
     val credentials: Credentials,
+)
+
+internal data class S3ExpressCredentialsCacheValue(
+    val expiringCredentials: ExpiringValue<Credentials>,
+    var usedSinceLastRefresh: Boolean
 )
