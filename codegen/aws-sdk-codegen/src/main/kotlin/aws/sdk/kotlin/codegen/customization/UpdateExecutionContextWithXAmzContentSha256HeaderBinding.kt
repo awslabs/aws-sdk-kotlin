1b16455cf07a56c797f462417cb70fb9ee261807
Matas Lauzadis <lauzmata@amazon.com>
2024-02-16 17:03:17 -0500

    Customization for x-amz-content-sha256 header bindings

---
+++ codegen/aws-sdk-codegen/src/main/kotlin/aws/sdk/kotlin/codegen/customization/UpdateExecutionContextWithXAmzContentSha256HeaderBinding.kt
@@ -0,0 +1,42 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+package aws.sdk.kotlin.codegen.customization
+
+import software.amazon.smithy.kotlin.codegen.core.KotlinWriter
+import software.amazon.smithy.kotlin.codegen.core.RuntimeTypes
+import software.amazon.smithy.kotlin.codegen.core.defaultName
+import software.amazon.smithy.kotlin.codegen.core.withBlock
+import software.amazon.smithy.kotlin.codegen.integration.KotlinIntegration
+import software.amazon.smithy.kotlin.codegen.model.getTrait
+import software.amazon.smithy.kotlin.codegen.rendering.protocol.ProtocolGenerator
+import software.amazon.smithy.kotlin.codegen.rendering.protocol.ProtocolMiddleware
+import software.amazon.smithy.model.shapes.MemberShape
+import software.amazon.smithy.model.shapes.OperationShape
+import software.amazon.smithy.model.traits.HttpHeaderTrait
+
+class UpdateExecutionContextWithXAmzContentSha256HeaderBinding : KotlinIntegration {
+    override fun customizeMiddleware(
+        ctx: ProtocolGenerator.GenerationContext,
+        resolved: List<ProtocolMiddleware>
+    ): List<ProtocolMiddleware>  { return resolved + UpdateExecutionContextWithSha256HeaderBindingMiddleware() }
+}
+
+private class UpdateExecutionContextWithSha256HeaderBindingMiddleware : ProtocolMiddleware {
+    override val name: String = "UpdateExecutionContextWithXAmzContentSha256HeaderBinding"
+
+    override fun isEnabledFor(ctx: ProtocolGenerator.GenerationContext, op: OperationShape): Boolean = getXAmzContentSha256HeaderMember(ctx, op) != null
+
+    override fun render(ctx: ProtocolGenerator.GenerationContext, op: OperationShape, writer: KotlinWriter) {
+        val member = getXAmzContentSha256HeaderMember(ctx, op)!!
+        writer.withBlock("input.${member.defaultName()}?.let {", "}") {
+            writer.write("op.context[#T.#T] = #T.Precalculated(it)", RuntimeTypes.Auth.Signing.AwsSigningCommon.AwsSigningAttributes, RuntimeTypes.Auth.Signing.AwsSigningCommon.HashSpecification, RuntimeTypes.Auth.Signing.AwsSigningCommon.HashSpecification)
+        }
+    }
+
+    private fun getXAmzContentSha256HeaderMember(ctx: ProtocolGenerator.GenerationContext, op: OperationShape): MemberShape? {
+        val input = ctx.model.expectShape(op.inputShape)
+        return input.members().singleOrNull { it.getTrait<HttpHeaderTrait>()?.value?.equals("x-amz-content-sha256") ?: false }
+    }
+}
