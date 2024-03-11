786ce037f9ee896dab6022ab9ebe9f3f42987358
0marperez <60363173+0marperez@users.noreply.github.com>
2024-03-11 09:48:49 -0600

    fix: add UnsupportedSigningAlgorithmInterceptor to all sigV4a services (#1245)

--- codegen/aws-sdk-codegen/src/main/kotlin/aws/sdk/kotlin/codegen/customization/s3/UnsupportedSigningAlgorithmIntegration.kt
+++ codegen/aws-sdk-codegen/src/main/kotlin/aws/sdk/kotlin/codegen/customization/s3/UnsupportedSigningAlgorithmIntegration.kt
@@ -5,25 +5,28 @@
 package aws.sdk.kotlin.codegen.customization.s3
 
 import aws.sdk.kotlin.codegen.AwsRuntimeTypes
+import software.amazon.smithy.aws.traits.auth.SigV4ATrait
 import software.amazon.smithy.kotlin.codegen.KotlinSettings
 import software.amazon.smithy.kotlin.codegen.core.KotlinWriter
 import software.amazon.smithy.kotlin.codegen.integration.KotlinIntegration
 import software.amazon.smithy.kotlin.codegen.rendering.protocol.ProtocolGenerator
 import software.amazon.smithy.kotlin.codegen.rendering.protocol.ProtocolMiddleware
 import software.amazon.smithy.model.Model
+import software.amazon.smithy.model.knowledge.ServiceIndex
 import software.amazon.smithy.model.shapes.OperationShape
 
 // FIXME: Remove this once sigV4a is supported by default AWS signer
 /**
- * Registers an interceptor for S3 to deal with the default signer not supporting sigV4a
+ * Registers an interceptor for sigV4a services to deal with the default signer not supporting sigV4a
  * See: [aws.sdk.kotlin.runtime.http.interceptors.UnsupportedSigningAlgorithmInterceptor]
  */
 class UnsupportedSigningAlgorithmIntegration : KotlinIntegration {
     override fun enabledForService(model: Model, settings: KotlinSettings): Boolean =
-        when (settings.sdkId.lowercase()) {
-            "s3", "eventbridge", "cloudfront keyvaluestore" -> true
-            else -> false
-        }
+        ServiceIndex
+            .of(model)
+            .getAuthSchemes(settings.service)
+            .values
+            .any { it.javaClass == SigV4ATrait::class.java }
 
     override fun customizeMiddleware(
         ctx: ProtocolGenerator.GenerationContext,
