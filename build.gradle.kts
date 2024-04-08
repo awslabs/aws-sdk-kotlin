3656526a5aa4c15b534912856921b4ca11b23ee6
0marperez <operez899999@gmail.com>
2024-04-08 12:26:00 -0400

    Add and configure artifact size metrics plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,7 +22,13 @@ plugins {
     // ensure the correct version of KGP ends up on our buildscript classpath
     alias(libs.plugins.kotlin.multiplatform) apply false
     alias(libs.plugins.kotlin.jvm) apply false
-    id("sdk-metrics")
+    id("artifact-size-metrics") version "1.0" // TODO: CHANGE TO GET IT FROM REMOTE REPO
+}
+
+artifactSizeMetrics {
+    artifactPrefixes = setOf(":services", ":aws-runtime")
+    closurePrefixes = setOf(":services")
+    significantChangeThresholdPercentage = 5.0
 }
 
 val testJavaVersion = typedProp<String>("test.java.version")?.let {
