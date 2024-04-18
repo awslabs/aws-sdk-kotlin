3d9959de4c0af963b14cd1a5335a7cf94145ffb8
Matas Lauzadis <lauzmata@amazon.com>
2024-04-18 15:59:09 -0400

    log a warning message

--- services/s3/common/src/aws/sdk/kotlin/services/s3/internal/ExpiresFieldInterceptor.kt
+++ services/s3/common/src/aws/sdk/kotlin/services/s3/internal/ExpiresFieldInterceptor.kt
@@ -4,13 +4,16 @@
  */
 package aws.sdk.kotlin.services.s3.internal
 
+import aws.smithy.kotlin.runtime.awsprotocol.ClockSkewInterceptor
 import aws.smithy.kotlin.runtime.client.ProtocolResponseInterceptorContext
 import aws.smithy.kotlin.runtime.http.interceptors.HttpInterceptor
 import aws.smithy.kotlin.runtime.http.request.HttpRequest
 import aws.smithy.kotlin.runtime.http.request.toBuilder
 import aws.smithy.kotlin.runtime.http.response.HttpResponse
 import aws.smithy.kotlin.runtime.http.response.toBuilder
+import aws.smithy.kotlin.runtime.telemetry.logging.logger
 import aws.smithy.kotlin.runtime.time.Instant
+import kotlin.coroutines.coroutineContext
 
 /**
  * Interceptor to handle special-cased `Expires` field which must not cause deserialization to fail.
@@ -26,6 +29,9 @@ internal object ExpiresFieldInterceptor : HttpInterceptor {
             try {
                 Instant.fromRfc5322(response.headers["Expires"]!!)
             } catch (e: Exception) {
+                coroutineContext.logger<ExpiresFieldInterceptor>().warn {
+                    "Failed to parse `expires`=${response.headers["Expires"]} as a timestamp, setting it to `null`. The unparsed value is available in `expiresString`."
+                }
                 response.headers.remove("Expires")
             }
         }
