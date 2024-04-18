b74f25b3ddbc7b226aef12902d923a9e7c3412bb
Matas Lauzadis <lauzmata@amazon.com>
2024-04-18 15:18:38 -0400

    Cleanup and only apply deprecation / ExpiresString to shapes with `OutputTrait`

--- codegen/aws-sdk-codegen/src/main/kotlin/aws/sdk/kotlin/codegen/customization/s3/S3ExpiresIntegration.kt
+++ codegen/aws-sdk-codegen/src/main/kotlin/aws/sdk/kotlin/codegen/customization/s3/S3ExpiresIntegration.kt
@@ -7,10 +7,7 @@ package aws.sdk.kotlin.codegen.customization.s3
 import software.amazon.smithy.kotlin.codegen.KotlinSettings
 import software.amazon.smithy.kotlin.codegen.core.KotlinWriter
 import software.amazon.smithy.kotlin.codegen.integration.KotlinIntegration
-import software.amazon.smithy.kotlin.codegen.model.buildSymbol
-import software.amazon.smithy.kotlin.codegen.model.expectShape
-import software.amazon.smithy.kotlin.codegen.model.getTrait
-import software.amazon.smithy.kotlin.codegen.model.shapes
+import software.amazon.smithy.kotlin.codegen.model.*
 import software.amazon.smithy.kotlin.codegen.rendering.protocol.ProtocolGenerator
 import software.amazon.smithy.kotlin.codegen.rendering.protocol.ProtocolMiddleware
 import software.amazon.smithy.model.Model
@@ -18,44 +15,45 @@ import software.amazon.smithy.model.shapes.*
 import software.amazon.smithy.model.traits.DeprecatedTrait
 import software.amazon.smithy.model.traits.DocumentationTrait
 import software.amazon.smithy.model.traits.HttpHeaderTrait
+import software.amazon.smithy.model.traits.OutputTrait
 import software.amazon.smithy.model.transform.ModelTransformer
 import kotlin.streams.asSequence
 
 /**
- * An integration used to customize behavior around S3's output members named `Expires`.
+ * An integration used to customize behavior around S3's members named `Expires`.
  */
 class S3ExpiresIntegration : KotlinIntegration {
     override fun enabledForService(model: Model, settings: KotlinSettings) =
         model.expectShape<ServiceShape>(settings.service).isS3 && model.shapes<OperationShape>().any { it.hasExpiresMember(model) }
 
-    // Ensure `Expires` is a deprecated timestamp type and add a new synthetic member `ExpiresString`
     override fun preprocessModel(model: Model, settings: KotlinSettings): Model {
         val transformer = ModelTransformer.create()
 
-        // Ensure all targets of `Expires` shapes are timestamps
-        val expiresMemberShapes = model.shapes()
+        // Ensure all `Expires` shapes are timestamps
+        val expiresShapeTimestampMap = model.shapes()
             .asSequence()
             .mapNotNull { shape ->
-                shape.members().firstOrNull { member -> member.memberName.lowercase() == "expires" }
+                shape.members()
+                    .singleOrNull { member -> member.memberName.lowercase() == "expires" }
+                    ?.target
             }
-            .map { it.target }
             .associateWith { ShapeType.TIMESTAMP }
 
-        var transformedModel = transformer.changeShapeType(model, expiresMemberShapes)
+        var transformedModel = transformer.changeShapeType(model, expiresShapeTimestampMap)
 
-        // Add a base `ExpiresString` shape
+        // Add an `ExpiresString` string shape to the model
         val expiresString = StringShape.builder()
         expiresString.id("aws.sdk.kotlin.s3.synthetic#ExpiresString")
         transformedModel = transformedModel.toBuilder().addShape(expiresString.build()).build()
 
-        // Deprecate `Expires` and add a synthetic member that targets `ExpiresString`
+        // For output shapes only, deprecate `Expires` and add a synthetic member that targets `ExpiresString`
         return transformer.mapShapes(transformedModel) { shape ->
-            if (shape.memberNames.any { it.lowercase() == "expires" }) {
-                check(shape is StructureShape) { "expected shape ${shape.id} with `Expires` member to be a StructureShape" }
-                val builder = shape.toBuilder()
+            if (shape.hasTrait<OutputTrait>() && shape.memberNames.any { it.lowercase() == "expires" }) {
+                val builder = (shape as StructureShape).toBuilder()
 
                 // Deprecate `Expires`
-                val expiresMember = shape.members().first { it.memberName.lowercase() == "expires" }
+                val expiresMember = shape.members().single { it.memberName.lowercase() == "expires" }
+
                 builder.removeMember(expiresMember.memberName)
                 val deprecatedTrait = DeprecatedTrait.builder()
                     .message("Please use `expiresString` which contains the raw, unparsed value of this field.")
@@ -73,7 +71,9 @@ class S3ExpiresIntegration : KotlinIntegration {
                 expiresStringMember.target(expiresString.id)
                 expiresStringMember.id(expiresMember.id.toString() + "String") // i.e. com.amazonaws.s3.<MEMBER_NAME>$ExpiresString
                 expiresStringMember.addTrait(HttpHeaderTrait("Expires")) // Add HttpHeaderTrait to ensure the field is deserialized
-                expiresStringMember.addTrait(expiresMember.getTrait<DocumentationTrait>()) // Copy documentation from `Expires`
+                expiresMember.getTrait<DocumentationTrait>()?.let {
+                    expiresStringMember.addTrait(it) // Copy documentation from `Expires`
+                }
                 builder.addMember(expiresStringMember.build())
                 builder.build()
             } else shape
@@ -85,7 +85,7 @@ class S3ExpiresIntegration : KotlinIntegration {
         resolved: List<ProtocolMiddleware>
     ): List<ProtocolMiddleware> = resolved + applyExpiresFieldInterceptor
 
-    private val applyExpiresFieldInterceptor = object : ProtocolMiddleware {
+    internal val applyExpiresFieldInterceptor = object : ProtocolMiddleware {
         override val name: String = "ExpiresFieldInterceptor"
 
         override fun isEnabledFor(ctx: ProtocolGenerator.GenerationContext, op: OperationShape): Boolean =
