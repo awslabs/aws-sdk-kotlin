5257833a400fe74c5739b10a81d941b4ddbc5c75
0marperez <operez899999@gmail.com>
2024-04-26 13:43:19 -0400

    Restore repo regex filter and address plugin ID change

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -11,6 +11,9 @@ pluginManagement {
         maven {
             name = "kotlinRepoTools"
             url = java.net.URI("https://d2gys1nrxnjnyg.cloudfront.net/releases")
+            content {
+                includeGroupByRegex("""aws\.sdk\.kotlin.*""")
+            }
         }
     }
 }
