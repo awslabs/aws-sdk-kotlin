c180c6f94f874ddcc7e68f2a0b9037b0575c8269
Matas Lauzadis <lauzmata@amazon.com>
2024-06-19 22:18:53 -0500

    Add `@JvmOverloads`

--- aws-runtime/aws-endpoint/common/src/aws/sdk/kotlin/runtime/endpoint/functions/Functions.kt
+++ aws-runtime/aws-endpoint/common/src/aws/sdk/kotlin/runtime/endpoint/functions/Functions.kt
@@ -9,6 +9,7 @@ import aws.sdk.kotlin.runtime.InternalSdkApi
 import aws.smithy.kotlin.runtime.client.endpoints.functions.isValidHostLabel
 import aws.smithy.kotlin.runtime.net.isIpv4
 import aws.smithy.kotlin.runtime.net.isIpv6
+import kotlin.jvm.JvmOverloads
 
 // the number of top-level components an arn contains (separated by colons)
 private const val ARN_COMPONENT_COUNT = 6
@@ -61,7 +62,7 @@ public data class Partition(
  * the result of a partition call.
  */
 @InternalSdkApi
-public data class PartitionConfig(
+public data class PartitionConfig @JvmOverloads constructor(
     public val name: String? = null,
     public val dnsSuffix: String? = null,
     public val dualStackDnsSuffix: String? = null,
