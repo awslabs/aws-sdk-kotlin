3f566a6d6ec348845a71cb5119a4902e70e9debf
Matas Lauzadis <lauzmata@amazon.com>
2024-08-13 12:42:16 -0400

    Replace #L with #T where appropriate

--- hll/codegen/src/main/kotlin/aws/sdk/kotlin/hll/codegen/rendering/BuilderRenderer.kt
+++ hll/codegen/src/main/kotlin/aws/sdk/kotlin/hll/codegen/rendering/BuilderRenderer.kt
@@ -1,9 +1,7 @@
 package aws.sdk.kotlin.hll.codegen.rendering
 
-import com.google.devtools.ksp.symbol.KSClassDeclaration
-import com.google.devtools.ksp.symbol.KSName
-import com.google.devtools.ksp.symbol.KSPropertyDeclaration
-import com.google.devtools.ksp.symbol.Nullability
+import aws.sdk.kotlin.hll.codegen.model.Type
+import com.google.devtools.ksp.symbol.*
 
 /**
  * A DSL-style builder renderer.
@@ -12,25 +10,27 @@ import com.google.devtools.ksp.symbol.Nullability
  */
 class BuilderRenderer(
     private val renderer: RendererBase,
-    classDeclaration: KSClassDeclaration,
+    private val classDeclaration: KSClassDeclaration,
 ) {
     private val properties = classDeclaration.getAllProperties().mapNotNull(KSClassProperty.Companion::from)
 
     private val className = classDeclaration.qualifiedName!!.getShortName()
-    private val builderName = "${className}Builder"
+    private val classType = Type.from(checkNotNull(classDeclaration.primaryConstructor?.returnType ) {
+        "Failed to determine class type for $className"
+    })
 
     fun render() {
         renderer.withDocs {
             renderer.write("A DSL-style builder for instances of [#L]", className)
         }
 
-        renderer.withBlock("public class #L {", "}", builderName) {
+        renderer.withBlock("public class #L {", "}", "${className}Builder") {
             properties.forEach {
                 renderer.write("public var #L: #L? = null", it.name, it.typeName.asString())
             }
             renderer.blankLine()
 
-            renderer.withBlock("public fun build(): #L {", "}", className) {
+            renderer.withBlock("public fun build(): #T {", "}", classType) {
                 properties.forEach {
                     if (it.nullable) {
                         renderer.write("val #1L = #1L", it.name)
@@ -39,7 +39,7 @@ class BuilderRenderer(
                     }
                 }
                 renderer.blankLine()
-                renderer.withBlock("return #L(", ")", className) {
+                renderer.withBlock("return #T(", ")", classType) {
                     properties.forEach {
                         renderer.write("#L,", it.name)
                     }
@@ -53,7 +53,7 @@ class BuilderRenderer(
 private data class KSClassProperty(val name: String, val typeName: KSName, val nullable: Boolean) {
     companion object {
         fun from(ksProperty: KSPropertyDeclaration): KSClassProperty? {
-            val type = ksProperty.getter?.returnType?.resolve() ?: return null
+            val type: KSType = ksProperty.getter?.returnType?.resolve() ?: return null
 
             val name = ksProperty.simpleName.getShortName()
             val typeName = type.declaration.qualifiedName ?: return null
