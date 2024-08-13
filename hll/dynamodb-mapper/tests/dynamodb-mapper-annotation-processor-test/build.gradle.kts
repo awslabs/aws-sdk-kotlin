7ea934e810aa69956eb5e4ec336fccc0fd0318c3
Matas Lauzadis <lauzmata@amazon.com>
2024-08-13 16:46:16 -0400

    Temporarily disable annotation-processor-test

--- hll/dynamodb-mapper/tests/dynamodb-mapper-annotation-processor-test/build.gradle.kts
+++ hll/dynamodb-mapper/tests/dynamodb-mapper-annotation-processor-test/build.gradle.kts
@@ -18,9 +18,9 @@ kotlin {
     }
 }
 
-dependencies {
-    listOf(
-        "kspCommonMainMetadata",
-        "kspJvm", // FIXME Generating common code is hard for KSP: https://github.com/google/ksp/issues/567
-    ).forEach { configuration -> add(configuration, project(":hll:dynamodb-mapper:dynamodb-mapper-codegen")) }
-}
+//dependencies {
+//    listOf(
+//        "kspCommonMainMetadata",
+//        "kspJvm", // FIXME Generating common code is hard for KSP: https://github.com/google/ksp/issues/567
+//    ).forEach { configuration -> add(configuration, project(":hll:dynamodb-mapper:dynamodb-mapper-codegen")) }
+//}
