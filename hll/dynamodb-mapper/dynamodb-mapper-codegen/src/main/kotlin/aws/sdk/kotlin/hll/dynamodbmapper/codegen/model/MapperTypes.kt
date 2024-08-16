1da7f9afa52d24bfac730e9fe776d2d7859ebb48
Matas Lauzadis <lauzmata@amazon.com>
2024-08-16 11:10:10 -0400

    Remove unused types

--- hll/dynamodb-mapper/dynamodb-mapper-codegen/src/main/kotlin/aws/sdk/kotlin/hll/dynamodbmapper/codegen/model/MapperTypes.kt
+++ hll/dynamodb-mapper/dynamodb-mapper-codegen/src/main/kotlin/aws/sdk/kotlin/hll/dynamodbmapper/codegen/model/MapperTypes.kt
@@ -20,13 +20,10 @@ object MapperTypes {
     object Items {
         fun itemSchema(typeVar: String) = TypeRef(Pkg.Hl.Items, "ItemSchema", listOf(TypeVar(typeVar)))
         fun itemSchemaPartitionKey(objectType: TypeRef, keyType: TypeRef) = TypeRef(Pkg.Hl.Items, "ItemSchema.PartitionKey", listOf(objectType, keyType))
-        val KeySpec = TypeRef(Pkg.Hl.Items, "KeySpec")
         fun keySpec(keyType: TypeRef) = TypeRef(Pkg.Hl.Items, "KeySpec", genericArgs = listOf(keyType))
         val KeySpecNumber = TypeRef(Pkg.Hl.Items, "KeySpec.Number")
         val KeySpecString = TypeRef(Pkg.Hl.Items, "KeySpec.String")
-        val ItemSchema = TypeRef(Pkg.Hl.Items, "ItemSchema")
         val AttributeDescriptor = TypeRef(Pkg.Hl.Items, "AttributeDescriptor")
-        val ItemConverter = TypeRef(Pkg.Hl.Items, "ItemConverter")
         fun itemConverter(objectType: TypeRef) = TypeRef(Pkg.Hl.Items, "ItemConverter", genericArgs = listOf(objectType))
         val SimpleItemConverter = TypeRef(Pkg.Hl.Items, "SimpleItemConverter")
     }
