7c9272ffe0af84870bce5400d3010b49707650c2
Matas Lauzadis <lauzmata@amazon.com>
2024-08-21 13:30:46 -0400

    Clean up

--- hll/dynamodb-mapper/dynamodb-mapper-schema-generator-plugin/build.gradle.kts
+++ hll/dynamodb-mapper/dynamodb-mapper-schema-generator-plugin/build.gradle.kts
@@ -56,38 +56,3 @@ tasks.test {
         exceptionFormat = org.gradle.api.tasks.testing.logging.TestExceptionFormat.FULL
     }
 }
-
-/**
- * FIXME This fixes the following error seen while trying to publish the plugin.
- *
- * * What went wrong:
- * A problem was found with the configuration of task ':hll:dynamodb-mapper:dynamodb-mapper-schema-generator-plugin:javadocJar' (type 'Jar').
- *   - Gradle detected a problem with the following location: '/Users/lauzmata/aws-sdk-kotlin/hll/dynamodb-mapper/dynamodb-mapper-schema-generator-plugin/build/libs/dynamodb-mapper-schema-generator-plugin-1.3.2-SNAPSHOT-javadoc.jar'.
- *
- *     Reason: Task ':hll:dynamodb-mapper:dynamodb-mapper-schema-generator-plugin:publishDynamodb-mapper-schema-generatorPluginMarkerMavenPublicationToMavenLocal' uses this output of task ':hll:dynamodb-mapper:dynamodb-mapper-schema-generator-plugin:javadocJar' without declaring an explicit or implicit dependency. This can lead to incorrect results being produced, depending on what order the tasks are executed.
- *
- *     Possible solutions:
- *       1. Declare task ':hll:dynamodb-mapper:dynamodb-mapper-schema-generator-plugin:javadocJar' as an input of ':hll:dynamodb-mapper:dynamodb-mapper-schema-generator-plugin:publishDynamodb-mapper-schema-generatorPluginMarkerMavenPublicationToMavenLocal'.
- *       2. Declare an explicit dependency on ':hll:dynamodb-mapper:dynamodb-mapper-schema-generator-plugin:javadocJar' from ':hll:dynamodb-mapper:dynamodb-mapper-schema-generator-plugin:publishDynamodb-mapper-schema-generatorPluginMarkerMavenPublicationToMavenLocal' using Task#dependsOn.
- *       3. Declare an explicit dependency on ':hll:dynamodb-mapper:dynamodb-mapper-schema-generator-plugin:javadocJar' from ':hll:dynamodb-mapper:dynamodb-mapper-schema-generator-plugin:publishDynamodb-mapper-schema-generatorPluginMarkerMavenPublicationToMavenLocal' using Task#mustRunAfter.
- *
- *     For more information, please refer to https://docs.gradle.org/8.5/userguide/validation_problems.html#implicit_dependency in the Gradle documentation.
- */
-afterEvaluate {
-    tasks.named("publishDynamodb-mapper-schema-generatorPluginMarkerMavenPublicationToMavenLocal").configure {
-        tasks.withType<Jar>().forEach {
-            if (it.name.contains("javadocJar")) {
-                dependsOn(it)
-            }
-        }
-    }
-    tasks.named("generateMetadataFileForPluginMavenPublication").configure {
-        tasks.withType<Jar>().forEach {
-            if (it.name.contains("emptyJar")) {
-                println("generateMetadataFileForPluginMavenPublication dependsOn ${it.name}")
-                dependsOn(it)
-            }
-        }
-    }
-}
-
