9642be3e2c78966c29dbb1b91ee9b3605c850ec7
Matas Lauzadis <lauzmata@amazon.com>
2024-08-22 14:53:19 -0400

    Add a test package which consumes the plugin

---
+++ hll/dynamodb-mapper/tests/dynamodb-mapper-schema-generator-plugin-test/common/test/aws/sdk/kotlin/hll/dynamodbmapper/tests/plugins/UserTest.kt
@@ -0,0 +1,33 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+package aws.sdk.kotlin.hll.dynamodbmapper.tests.plugins
+
+import aws.sdk.kotlin.hll.dynamodbmapper.model.itemOf
+import aws.sdk.kotlin.hll.dynamodbmapper.tests.plugins.mapper.schemas.UserConverter
+import aws.sdk.kotlin.services.dynamodb.model.AttributeValue
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class UserTest {
+    @Test
+    fun testConversion() {
+        val user = User(123, "Steve", "Rogers", 84)
+        val converted = UserConverter.toItem(user)
+
+        assertEquals(
+            itemOf(
+                "id" to AttributeValue.N("123"),
+                "fName" to AttributeValue.S("Steve"),
+                "lName" to AttributeValue.S("Rogers"),
+                "age" to AttributeValue.N("84"),
+            ),
+            converted,
+        )
+
+        val unconverted = UserConverter.fromItem(converted)
+
+        assertEquals(user, unconverted)
+    }
+}
