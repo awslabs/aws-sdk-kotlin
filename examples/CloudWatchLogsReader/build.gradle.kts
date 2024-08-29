86028b88cac87711d877e74465cb4eb9fc875216
kashif Mehmood <kashismails@gmail.com>
2024-08-29 22:23:20 +0500

    initial configuration for cloud watch reader example

---
+++ examples/CloudWatchLogsReader/build.gradle.kts
@@ -0,0 +1,21 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+plugins {
+    kotlin("jvm")
+    application
+}
+
+application {
+    mainClass.set("aws.sdk.kotlin.example.MainKt")
+}
+
+val awsSdkKotlinVersion: String by project
+
+dependencies {
+    implementation(kotlin("stdlib"))
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.4.3")
+    implementation("aws.sdk.kotlin:cloudwatchlogs:$awsSdkKotlinVersion")
+    implementation("com.google.code.gson:gson:2.8.9")
+}
---
+++ examples/CloudWatchLogsReader/src/main/kotlin/Main.kt
@@ -0,0 +1,9 @@
+package aws.sdk.kotlin.example
+
+import kotlinx.coroutines.runBlocking
+
+
+fun main() = runBlocking {
+   println("cloud watch reader")
+}
+
