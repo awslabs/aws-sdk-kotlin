0318aa52b68c9e4eae66473215173a1c488cf43b
Ian Botsford <83236726+ianbotsf@users.noreply.github.com>
2024-09-18 18:47:14 +0000

    addressing PR feedback: abstract stdout replacement and double-invocation guarding to base symbol processor

--- hll/dynamodb-mapper/dynamodb-mapper-codegen/src/main/kotlin/aws/sdk/kotlin/hll/dynamodbmapper/codegen/operations/HighLevelOpsProcessor.kt
+++ hll/dynamodb-mapper/dynamodb-mapper-codegen/src/main/kotlin/aws/sdk/kotlin/hll/dynamodbmapper/codegen/operations/HighLevelOpsProcessor.kt
@@ -5,10 +5,9 @@
 package aws.sdk.kotlin.hll.dynamodbmapper.codegen.operations
 
 import aws.sdk.kotlin.hll.codegen.core.CodeGeneratorFactory
+import aws.sdk.kotlin.hll.codegen.ksp.processors.HllKspProcessor
 import aws.sdk.kotlin.hll.codegen.model.Operation
 import aws.sdk.kotlin.hll.codegen.rendering.RenderContext
-import aws.sdk.kotlin.hll.codegen.util.KspLoggerOutputStream
-import aws.sdk.kotlin.hll.codegen.util.asPrintStream
 import aws.sdk.kotlin.hll.dynamodbmapper.codegen.model.MapperPkg
 import aws.sdk.kotlin.hll.dynamodbmapper.codegen.operations.model.toHighLevel
 import aws.sdk.kotlin.hll.dynamodbmapper.codegen.operations.rendering.HighLevelRenderer
@@ -24,28 +23,20 @@ import com.google.devtools.ksp.symbol.KSFunctionDeclaration
  * symbols to use as codegen input (i.e., the low-level DynamoDB operations/types), wiring up rendering context, and
  * starting the top-level renderer (which may in turn call other renderers).
  */
-internal class HighLevelOpsProcessor(environment: SymbolProcessorEnvironment) : SymbolProcessor {
+internal class HighLevelOpsProcessor(environment: SymbolProcessorEnvironment) : HllKspProcessor(environment) {
     private val codeGenerator = environment.codeGenerator
-    private var invoked = false
     private val logger = environment.logger
     private val opAllowlist = environment.options["op-allowlist"]?.split(";")
     private val pkg = environment.options["pkg"] ?: MapperPkg.Hl.Ops
 
-    override fun process(resolver: Resolver): List<KSAnnotated> {
-        if (!invoked) {
-            invoked = true
+    override fun processImpl(resolver: Resolver): List<KSAnnotated> {
+        logger.info("Scanning low-level DDB client for operations and types")
+        val operations = getOperations(resolver)
 
-            val loggerOutStream = KspLoggerOutputStream(logger)
-            System.setOut(loggerOutStream.asPrintStream())
+        val codegenFactory = CodeGeneratorFactory(codeGenerator, logger)
+        val ctx = RenderContext(logger, codegenFactory, pkg, "dynamodb-mapper-ops-codegen")
 
-            logger.info("Scanning low-level DDB client for operations and types")
-            val operations = getOperations(resolver)
-
-            val codegenFactory = CodeGeneratorFactory(codeGenerator, logger)
-            val ctx = RenderContext(logger, codegenFactory, pkg, "dynamodb-mapper-ops-codegen")
-
-            HighLevelRenderer(ctx, operations).render()
-        }
+        HighLevelRenderer(ctx, operations).render()
 
         return listOf()
     }
