10044f6f588d2d1ca350ebc42ca966cf87708a30
Matas Lauzadis <lauzmata@amazon.com>
2024-09-19 09:18:56 -0400

    Add KDocs

--- hll/dynamodb-mapper/dynamodb-mapper/common/src/aws/sdk/kotlin/hll/dynamodbmapper/values/ItemToValueConverter.kt
+++ hll/dynamodb-mapper/dynamodb-mapper/common/src/aws/sdk/kotlin/hll/dynamodbmapper/values/ItemToValueConverter.kt
@@ -8,7 +8,18 @@ import aws.sdk.kotlin.hll.dynamodbmapper.model.Item
 import aws.sdk.kotlin.hll.dynamodbmapper.model.toItem
 import aws.sdk.kotlin.services.dynamodb.model.AttributeValue
 
+/**
+ * Converts between [Item] and [AttributeValue].
+ * This converter is typically chained following an [ItemConverter] using the [andThenTo] extension function.
+ */
 public object ItemToValueConverter : ValueConverter<Item> {
+    /**
+     * Convert from [AttributeValue] to [Item]
+     */
     override fun convertFrom(to: AttributeValue): Item = to.asM().toItem()
+
+    /**
+     * Convert [from] [Item] to [AttributeValue]
+     */
     override fun convertTo(from: Item): AttributeValue = AttributeValue.M(from)
 }
