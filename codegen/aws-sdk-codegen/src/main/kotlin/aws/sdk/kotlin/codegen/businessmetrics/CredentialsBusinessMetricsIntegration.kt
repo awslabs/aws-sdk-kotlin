2a824b82ba26b99a39b5f39b4770f799594bbca4
Matas Lauzadis <lauzmata@amazon.com>
2024-11-26 13:46:12 -0600

    Fix emission of Credentials business metrics

--- codegen/aws-sdk-codegen/src/main/kotlin/aws/sdk/kotlin/codegen/businessmetrics/CredentialsBusinessMetricsIntegration.kt
+++ codegen/aws-sdk-codegen/src/main/kotlin/aws/sdk/kotlin/codegen/businessmetrics/CredentialsBusinessMetricsIntegration.kt
@@ -1,18 +1,32 @@
 package aws.sdk.kotlin.codegen.businessmetrics
 
 import aws.sdk.kotlin.codegen.AwsRuntimeTypes
+import software.amazon.smithy.aws.traits.auth.SigV4ATrait
+import software.amazon.smithy.aws.traits.auth.SigV4Trait
+import software.amazon.smithy.kotlin.codegen.KotlinSettings
 import software.amazon.smithy.kotlin.codegen.core.KotlinWriter
 import software.amazon.smithy.kotlin.codegen.core.RuntimeTypes
 import software.amazon.smithy.kotlin.codegen.core.withBlock
 import software.amazon.smithy.kotlin.codegen.integration.KotlinIntegration
 import software.amazon.smithy.kotlin.codegen.rendering.protocol.ProtocolGenerator
 import software.amazon.smithy.kotlin.codegen.rendering.protocol.ProtocolMiddleware
+import software.amazon.smithy.model.Model
+import software.amazon.smithy.model.knowledge.ServiceIndex
 import software.amazon.smithy.model.shapes.OperationShape
 
 /**
  * Renders the addition of some of the credentials related business metrics.
  */
 class CredentialsBusinessMetricsIntegration : KotlinIntegration {
+    override fun enabledForService(model: Model, settings: KotlinSettings): Boolean {
+        val serviceIndex = ServiceIndex.of(model)
+        val schemes = serviceIndex.getAuthSchemes(settings.service)
+
+        return schemes.values.any {
+            it.javaClass == SigV4ATrait::class.java || it.javaClass == SigV4Trait::class.java
+        }
+    }
+
     override fun customizeMiddleware(
         ctx: ProtocolGenerator.GenerationContext,
         resolved: List<ProtocolMiddleware>,
