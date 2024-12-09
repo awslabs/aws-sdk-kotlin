c83ea8d88fc745cf70ff378d1c649c0194ebe4c2
0marperez <operez899999@gmail.com>
2024-12-09 14:58:20 -0500

    misc: enable smoke tests

---
+++ codegen/aws-sdk-codegen/src/main/kotlin/aws/sdk/kotlin/codegen/smoketests/SmokeTestsDenyListIntegration.kt
@@ -0,0 +1,33 @@
+package aws.sdk.kotlin.codegen.smoketests
+
+import software.amazon.smithy.kotlin.codegen.KotlinSettings
+import software.amazon.smithy.kotlin.codegen.integration.KotlinIntegration
+import software.amazon.smithy.kotlin.codegen.integration.SectionWriterBinding
+import software.amazon.smithy.kotlin.codegen.rendering.smoketests.SmokeTestSectionIds.SmokeTestsFile
+import software.amazon.smithy.model.Model
+
+/**
+ * SDK ID's of services that are deny listed from smoke test code generation.
+ */
+val smokeTestDenyList = setOf(
+    "S3Tables",
+)
+
+/**
+ * Will wipe the smoke test runner file for services that are deny listed.
+ *
+ * Some services model smoke tests incorrectly and the code generated file will not compile.
+ */
+class SmokeTestsDenyListIntegration : KotlinIntegration {
+    override fun enabledForService(model: Model, settings: KotlinSettings): Boolean =
+        settings.sdkId in smokeTestDenyList
+
+    override val sectionWriters: List<SectionWriterBinding>
+        get() = listOf(
+            SectionWriterBinding(
+                SmokeTestsFile,
+            ) { writer, _ ->
+                writer.write("// Smoke tests for service deny listed")
+            },
+        )
+}
