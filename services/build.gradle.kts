8cde9fa8a6878f67aac5e46413de257f3b5d660c
Matas Lauzadis <lauzmata@amazon.com>
2025-08-20 10:14:22 -0400

    Enable allWarningsAsErrors

--- services/build.gradle.kts
+++ services/build.gradle.kts
@@ -129,7 +129,6 @@ subprojects {
 
     tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
         compilerOptions {
-            allWarningsAsErrors.set(false) // FIXME Tons of errors occur in generated code
             jvmTarget.set(JvmTarget.JVM_1_8) // fixes outgoing variant metadata: https://github.com/smithy-lang/smithy-kotlin/issues/258
             freeCompilerArgs.add("-Xjdk-release=1.8")
         }
